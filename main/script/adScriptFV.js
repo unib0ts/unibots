746e6efd054c5967d194552350bb80a8e165266c
sc0rp1on <singhsooraj.baliyan@gmail.com>
2020-03-09 13:42:04 +0530

    FV Ad

---
+++ main/script/adScriptFV.js
@@ -0,0 +1,69 @@
+var sizes = [[320, 50]];
+var PREBID_TIMEOUT = 2000;
+var FAILSAFE_TIMEOUT = 3000;
+var REFRESH_TIMEOUT = 20000;
+
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
+
+var adUnits = [{
+  code: '/21928950349/freshersvoice.com_NB_320x50',
+  mediaTypes: {
+	  banner: {
+		  sizes: sizes
+	  }
+  },
+  bids: [{
+	  bidder: 'appnexus',
+	  params: {
+		  placementId: 18677301
+	  }
+  }]
+}];
+
+var pbjs = pbjs || {};
+pbjs.que = pbjs.que || [];
+
+pbjs.que.push(function() {
+  pbjs.addAdUnits(adUnits);
+});
+
+var slot1;
+googletag.cmd.push(function() {
+  slot1 = googletag.defineSlot('/21928950349/freshersvoice.com_NB_320x50', sizes, 'div-gpt-ad-1583566570048-0')
+	.addService(googletag.pubads());
+  googletag.pubads().disableInitialLoad();
+  googletag.pubads().enableSingleRequest();
+  googletag.enableServices();
+});
+
+function refreshBid() {
+  pbjs.que.push(function() {
+	  pbjs.requestBids({
+		  timeout: PREBID_TIMEOUT,
+		  adUnitCodes: ['/21928950349/freshersvoice.com_NB_320x50'],
+		  bidsBackHandler: function() {
+			  pbjs.setTargetingForGPTAsync(['/21928950349/freshersvoice.com_NB_320x50']);
+			  googletag.pubads().refresh([slot1]);
+		  }
+	  });
+  });
+}
+
+function initAdserver() {
+  if (pbjs.initAdserverSet) return;
+  pbjs.initAdserverSet = true;
+  googletag.cmd.push(function() {
+	  pbjs.setTargetingForGPTAsync && pbjs.setTargetingForGPTAsync();
+	  googletag.pubads().refresh();
+  });
+}
+
+// in case PBJS doesn't load
+setTimeout(function() {
+  initAdserver();
+}, FAILSAFE_TIMEOUT);
+
+setInterval(function() {
+  refreshBid();
+}, REFRESH_TIMEOUT);

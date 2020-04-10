325b905c8acbea7143ef23310ed814165146bb4b
sc0rp1on <singhsooraj.baliyan@gmail.com>
2020-04-10 21:16:47 +0530

    TTN

--- main/script/adScriptTTN.js
+++ main/script/adScriptTTN.js
@@ -7,7 +7,7 @@ var googletag = googletag || {};
 googletag.cmd = googletag.cmd || [];
 
 var adUnits = [{
-  code: '/21928950349/toptamilnews.com_NB_320x50',
+  code: '/21956916242/toptamilnews.com_NB_320x50',
   mediaTypes: {
 	  banner: {
 		  sizes: sizes
@@ -30,34 +30,79 @@ ubpbjs.que = ubpbjs.que || [];
 
 ubpbjs.que.push(function() {
   ubpbjs.addAdUnits(adUnits);
-  ubpbjs.setConfig({ userSync: {
-            iframeEnabled: true
-         }
+  ubpbjs.setConfig({
+    userSync: {
+        iframeEnabled: true
+     },
+   "currency": {
+      // enables currency feature
+      "adServerCurrency": "AED",
+      "granularityMultiplier":3 ,
+      // optionally override the default rate file
+      "conversionRateFile": "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/currency/currency.json",
+      // optionally provide a default rate in case the file can't be read
+      "defaultRates": { "USD": { "AED": 3.67 }}
+    }
   });
   ubpbjs.requestBids({
     timeout: PREBID_TIMEOUT,
-    adUnitCodes: ['/21928950349/toptamilnews.com_NB_320x50'],
+    adUnitCodes: ['/21956916242/toptamilnews.com_NB_320x50'],
     bidsBackHandler: initAdserver
   });
 });
 
-ubpbjs.bidderSettings = {
-    oftmedia: {
-      bidCpmAdjustment: function(bidCpm){
-        return bidCpm*0.80;
-      }
-	},
-    emx_digital: {
-      bidCpmAdjustment: function(bidCpm){
-        return bidCpm*0.80;
-      }
-    }
-};
+// ubpbjs.bidderSettings = {
+//     oftmedia: {
+//       bidCpmAdjustment: function(bidCpm){
+//         return bidCpm*0.80;
+//       }
+// 	},
+//     emx_digital: {
+//       bidCpmAdjustment: function(bidCpm){
+//         return bidCpm*0.80;
+//       }
+//     }
+// };
 
+ubpbjs.bidderSettings = {
+    standard: {
+        adserverTargeting: [{
+            key: "hb_bidder",
+            val: function(bidResponse) {
+                return bidResponse.bidderCode;
+            }
+        }, {
+            key: "hb_adid",
+            val: function(bidResponse) {
+                return bidResponse.adId;
+            }
+        }, {
+            key: "hb_pb",
+            val: function(bidResponse) {
+                return bidResponse.pbHg;
+            }
+        }, {
+            key: 'hb_size',
+            val: function (bidResponse) {
+                return bidResponse.size;
+            }
+        }, {
+            key: 'hb_source',
+            val: function (bidResponse) {
+                return bidResponse.source;
+            }
+        }, {
+            key: 'hb_format',
+            val: function (bidResponse) {
+                return bidResponse.mediaType;
+            }
+        }]
+    }
+}
 
 var slot1;
 googletag.cmd.push(function() {
-  slot1 = googletag.defineSlot('/21928950349/toptamilnews.com_NB_320x50', sizes, 'div-ub-1')
+  slot1 = googletag.defineSlot('/21956916242/toptamilnews.com_NB_320x50', sizes, 'div-ub-1')
 	.addService(googletag.pubads());
   googletag.pubads().disableInitialLoad();
   googletag.pubads().enableSingleRequest();
@@ -68,9 +113,9 @@ function refreshBid() {
   ubpbjs.que.push(function() {
 	  ubpbjs.requestBids({
 		  timeout: PREBID_TIMEOUT,
-		  adUnitCodes: ['/21928950349/toptamilnews.com_NB_320x50'],
+		  adUnitCodes: ['/21956916242/toptamilnews.com_NB_320x50'],
 		  bidsBackHandler: function() {
-			  ubpbjs.setTargetingForGPTAsync(['/21928950349/toptamilnews.com_NB_320x50']);
+			  ubpbjs.setTargetingForGPTAsync(['/21956916242/toptamilnews.com_NB_320x50']);
 			  googletag.pubads().refresh([slot1]);
 		  }
 	  });

12793a0aae9271913b8eb724c2c6f1e805a974f7
sc0rp1on <singhsooraj.baliyan@gmail.com>
2020-05-10 21:47:17 +0530

    New Client Bidders

--- main/script/adScriptGoodnewsfromindonesia.js
+++ main/script/adScriptGoodnewsfromindonesia.js
@@ -18,19 +18,53 @@ var adUnits = [
     {
         code: '/21956916242/Goodnewsfromindonesia.id_nb_320x50',
         mediaTypes: {
+          native: {
+            image: {
+              // sizes: [300, 100],
+              aspect_ratios: [{
+                min_width: 100,        /* Optional */
+                min_height: 50,       /* Optional */
+                ratio_width: 2,        /* Required */
+                ratio_height: 1,       /* Required */
+              }],
+              sendId: true
+            },
+            title: {
+              len: 50,
+              sendId: true
+            },
+            sponsoredBy: {
+              required: true,
+              sendId: true
+            },
+            clickUrl: {
+              required: true,
+              sendId: true
+            }
+          }
+        },
+        bids: [
+          { bidder: 'appnexus', params: { placementId: '19201709' } }
+          // { bidder: 'oftmedia', params: { placementId: '19095410' } }
+        ]
+    },
+    {
+        code: '/21956916242/Goodnewsfromindonesia.id_nb_320x50',
+        mediaTypes: {
             banner: {
                 sizes: div_1_sizes
             }
         },
         bids: [
-        	// { bidder: 'appnexus', params: { placementId: '18926432' } }, /* one placementId for all sizes  my appnexus bidder */
+        	{ bidder: 'appnexus', params: { placementId: '19201709' } }, /* one placementId for all sizes  my appnexus bidder */
         	// { bidder: 'oftmedia', params: { placementId: '18671523' } },
         	// { bidder: '33across', params: { siteId : 'acSmlCBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
         	// { bidder: 'emx_digital', params: { tagid: '97458' } }, /* sizeless */
-          // { bidder: 'sovrn', params: {tagid: '701548'} },
+          { bidder: 'sovrn', params: {tagid: '721742'} },
           // { bidder: 'openx', params: {unit: '541046667', delDomain: 'yieldbird-d.openx.net'} },
         	// { bidder: 'rhythmone', params: { placementId: '205372' } }, /* one placementId for all sizes */
-        	{ bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } }
+        	{ bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
+          { bidder: 'criteo', params: {networkId: '4902'} }
         ]
     }
 ];

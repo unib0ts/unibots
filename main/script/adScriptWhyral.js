acd254a2dcb81d1c98099d7f91a7963ca4134cc1
sc0rp1on <singhsooraj.baliyan@gmail.com>
2020-05-15 02:36:26 +0530

    const to var

--- main/script/adScriptWhyral.js
+++ main/script/adScriptWhyral.js
@@ -2,7 +2,7 @@ var PREBID_TIMEOUT = 2000;
 var FAILSAFE_TIMEOUT = 3000;
 var REFRESH_TIMEOUT = 60000;
 
-const customConfigObjectA = {
+var customConfigObjectA = {
  "buckets" : [{
     "precision": 2,  //default is 2 if omitted - means 2.1234 rounded to 2 decimal places = 2.12
     "min" : 0,

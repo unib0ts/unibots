f87f51cbdb3acf1013047f25545b20e211dae70b
bhu15dr <bhu15dr@gmail.com>
2020-05-17 00:46:34 +0530

    adscript

--- main/script/adScriptTWL.js
+++ main/script/adScriptTWL.js
@@ -11,30 +11,30 @@ const customConfigObjectA = {
     }]
 };
 
-var div_1_sizes = [320, 50];
+// var div_1_sizes = [320, 50];
 var div_2_sizes = [300, 250];
 var div_3_sizes = [[200, 200], [300, 250], [250, 250]];
 var adUnits = [
-    {
-      code: '/21928950349/thewall.in_NB_320x50',
-        mediaTypes: {
-            banner: {
-                sizes: div_1_sizes
-            }
-        },
-        bids: [
-        	{ bidder: 'appnexus', params: { placementId: '19057843' } }, /* one placementId for all sizes  my appnexus bidder */
-        	//{ bidder: 'oftmedia', params: { placementId: '18671514' } },
-        	//{ bidder: '33across', params: { siteId : 'd9bktEBt8r6PWLaKlId8sQ:siab', productId: 'siab' } }, /*All sizes*/
-        	//{ bidder: 'emx_digital', params: { tagid: '97448' } }, /* sizeless */
-          { bidder: 'sovrn', params: {tagid: '714244'} },
-          { bidder: 'criteo', params: {networkId: '4902'} },
-          { bidder: 'openx', params: {unit: '541046564', delDomain: 'yieldbird-d.openx.net'} },
-        	{ bidder: 'rhythmone', params: { placementId: '205945' } }, /* one placementId for all sizes */
-          { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
-        	{ bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } }
-        ]
-    },
+    // {
+    //   code: '/21928950349/thewall.in_NB_320x50',
+    //     mediaTypes: {
+    //         banner: {
+    //             sizes: div_1_sizes
+    //         }
+    //     },
+    //     bids: [
+    //     	{ bidder: 'appnexus', params: { placementId: '19057843' } }, /* one placementId for all sizes  my appnexus bidder */
+    //     	//{ bidder: 'oftmedia', params: { placementId: '18671514' } },
+    //     	//{ bidder: '33across', params: { siteId : 'd9bktEBt8r6PWLaKlId8sQ:siab', productId: 'siab' } }, /*All sizes*/
+    //     	//{ bidder: 'emx_digital', params: { tagid: '97448' } }, /* sizeless */
+    //       { bidder: 'sovrn', params: {tagid: '714244'} },
+    //       { bidder: 'criteo', params: {networkId: '4902'} },
+    //       { bidder: 'openx', params: {unit: '541046564', delDomain: 'yieldbird-d.openx.net'} },
+    //     	{ bidder: 'rhythmone', params: { placementId: '205945' } }, /* one placementId for all sizes */
+    //       { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
+    //     	{ bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } }
+    //     ]
+    // },
     {
       code: '/21928950349/thewall.in_NB_300x250',
         mediaTypes: {
@@ -135,7 +135,7 @@ function initAdserver() {
     googletag.cmd.push(function() {
         ubpbjs.que.push(function() {
             ubpbjs.setTargetingForGPTAsync();
-            googletag.pubads().refresh([ub_slot1, ub_slot2, ub_slot3]);
+            googletag.pubads().refresh([ub_slot2, ub_slot3]);
         });
     });
 }
@@ -144,9 +144,9 @@ setTimeout(function() {
     initAdserver();
 }, FAILSAFE_TIMEOUT);
 
-var ub_slot1, ub_slot2, ub_slot3;
+var ub_slot2, ub_slot3;
 googletag.cmd.push(function() {
-    ub_slot1 = googletag.defineSlot('/21928950349/thewall.in_NB_320x50', div_1_sizes, 'div-ub-1').addService(googletag.pubads());
+    // ub_slot1 = googletag.defineSlot('/21928950349/thewall.in_NB_320x50', div_1_sizes, 'div-ub-1').addService(googletag.pubads());
     ub_slot2 = googletag.defineSlot('/21928950349/thewall.in_NB_300x250', div_2_sizes, 'div-ub-2').addService(googletag.pubads());
     ub_slot3 = googletag.defineSlot('/21928950349/thewall.in_nb_300x250_hb', div_3_sizes, 'div-gpt-ad-1589043268569-0').addService(googletag.pubads());
     googletag.pubads().collapseEmptyDivs(true);
@@ -155,17 +155,17 @@ googletag.cmd.push(function() {
     googletag.pubads().enableSingleRequest();
     googletag.enableServices();
     googletag.pubads().addEventListener('slotRenderEnded', function(event) {
-        if (event.slot === ub_slot1) {
-          ub_checkAd1Rendered();
-        }
-        else if (event.slot === ub_slot2) {
+        // if (event.slot === ub_slot1) {
+        //   ub_checkAd1Rendered();
+        // }
+        if (event.slot === ub_slot2) {
           ub_checkAd2Rendered();
         }
         else if (event.slot === ub_slot3) {
           ub_checkAd3Rendered();
         }
-        else if ((event.slot === ub_slot1) && (event.slot === ub_slot2) && (event.slot === ub_slot3)) {
-          ub_checkAd1Rendered();
+        else if ((event.slot === ub_slot2) && (event.slot === ub_slot3)) {
+          // ub_checkAd1Rendered();
           ub_checkAd2Rendered();
           ub_checkAd3Rendered();
         }
@@ -176,7 +176,7 @@ function refreshBid(ub_slot) {
   ubpbjs.que.push(function() {
 	  ubpbjs.requestBids({
 		  timeout: PREBID_TIMEOUT,
-		  adUnitCodes: ['/21928950349/thewall.in_NB_320x50', '/21928950349/thewall.in_NB_300x250', '/21928950349/thewall.in_nb_300x250_hb'],
+		  adUnitCodes: ['/21928950349/thewall.in_NB_300x250', '/21928950349/thewall.in_nb_300x250_hb'],
 		  bidsBackHandler: function() {
         googletag.cmd.push(function() {
           ubpbjs.que.push(function() {
@@ -189,19 +189,19 @@ function refreshBid(ub_slot) {
   });
 }
 
-ub_ad1RefreshFlag = 0;
-function ub_checkAd1Rendered(){
-	adId1 = 'div-ub-1';
-	var nodes = document.getElementById(adId1).childNodes[0].childNodes;
-	if(nodes.length && nodes[0].nodeName.toLowerCase() == 'iframe') {
-    if(ub_ad1RefreshFlag != 1){
-      setInterval(function() {
-        ub_ad1RefreshFlag = 1;
-        refreshBid(ub_slot1);
-      }, REFRESH_TIMEOUT);
-    }
-	 }
-}
+// ub_ad1RefreshFlag = 0;
+// function ub_checkAd1Rendered(){
+// 	adId1 = 'div-ub-1';
+// 	var nodes = document.getElementById(adId1).childNodes[0].childNodes;
+// 	if(nodes.length && nodes[0].nodeName.toLowerCase() == 'iframe') {
+//     if(ub_ad1RefreshFlag != 1){
+//       setInterval(function() {
+//         ub_ad1RefreshFlag = 1;
+//         refreshBid(ub_slot1);
+//       }, REFRESH_TIMEOUT);
+//     }
+// 	 }
+// }
 
 ub_ad2RefreshFlag = 0;
 function ub_checkAd2Rendered(){

d643ab2329ef82a7ca0127b1f48142e95971f8f2
bhu15dr <bhu15dr@gmail.com>
2020-05-17 16:38:17 +0530

    ad script

--- main/script/adScriptBGP.js
+++ main/script/adScriptBGP.js
@@ -11,63 +11,63 @@ const customConfigObjectA = {
     }]
 };
 
-// var div_1_sizes = [320, 50];
+var div_1_sizes = [320, 50];
 var div_2_sizes = [300, 250];
 var div_3_sizes = [728, 90];
 
 var adUnits = [
-    // {
-    //     code: '/21928950349/banglarpran.com_NB_320x50',
-    //     mediaTypes: {
-    //       native: {
-    //         image: {
-    //           // sizes: [300, 100],
-    //           aspect_ratios: [{
-    //             min_width: 100,        /* Optional */
-    //             min_height: 50,       /* Optional */
-    //             ratio_width: 2,        /* Required */
-    //             ratio_height: 1,       /* Required */
-    //           }],
-    //           sendId: true
-    //         },
-    //         title: {
-    //           len: 50,
-    //           sendId: true
-    //         },
-    //         sponsoredBy: {
-    //           required: true,
-    //           sendId: true
-    //         },
-    //         clickUrl: {
-    //           required: true,
-    //           sendId: true
-    //         }
-    //       }
-    //     },
-    //     bids: [
-    //       { bidder: 'appnexus', params: { placementId: '19057746' } }
-    //     ]
-    // },
-    // {
-    //     code: '/21928950349/banglarpran.com_NB_320x50',
-    //     mediaTypes: {
-    //         banner: {
-    //             sizes: div_1_sizes
-    //         }
-    //     },
-    //     bids: [
-    //     	{ bidder: 'appnexus', params: { placementId: '19057746' } }, /* one placementId for all sizes  my appnexus bidder */
-    //     	//{ bidder: 'oftmedia', params: { placementId: '18671514' } },
-    //     	//{ bidder: '33across', params: { siteId : 'bc-OPEBt8r6OkGaKkGJozW:siab', productId: 'siab' } }, /*All sizes*/
-    //     	{ bidder: 'emx_digital', params: { tagid: '103695' } }, /* sizeless */
-    //       { bidder: 'sovrn', params: {tagid: '713872'} },
-    //       { bidder: 'openx', params: {unit: '541046448', delDomain: 'yieldbird-d.openx.net'} },
-    //     	{ bidder: 'rhythmone', params: { placementId: '205945'} }, /* one placementId for all sizes */
-    //     	{ bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
-    //       { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
-    //       { bidder: 'criteo', params: {networkId: '4902'} }
-    //     ]
-    // },
+    {
+        code: '/21928950349/banglarpran.com_NB_320x50',
+        mediaTypes: {
+          native: {
+            image: {
+              // sizes: [300, 100],
+              aspect_ratios: [{
+                min_width: 100,        /* Optional */
+                min_height: 50,       /* Optional */
+                ratio_width: 2,        /* Required */
+                ratio_height: 1,       /* Required */
+              }],
+              sendId: true
+            },
+            title: {
+              len: 50,
+              sendId: true
+            },
+            sponsoredBy: {
+              required: true,
+              sendId: true
+            },
+            clickUrl: {
+              required: true,
+              sendId: true
+            }
+          }
+        },
+        bids: [
+          { bidder: 'appnexus', params: { placementId: '19057746' } }
+        ]
+    },
+    {
+        code: '/21928950349/banglarpran.com_NB_320x50',
+        mediaTypes: {
+            banner: {
+                sizes: div_1_sizes
+            }
+        },
+        bids: [
+        	{ bidder: 'appnexus', params: { placementId: '19057746' } }, /* one placementId for all sizes  my appnexus bidder */
+        	//{ bidder: 'oftmedia', params: { placementId: '18671514' } },
+        	//{ bidder: '33across', params: { siteId : 'bc-OPEBt8r6OkGaKkGJozW:siab', productId: 'siab' } }, /*All sizes*/
+        	{ bidder: 'emx_digital', params: { tagid: '103695' } }, /* sizeless */
+          { bidder: 'sovrn', params: {tagid: '713872'} },
+          { bidder: 'openx', params: {unit: '541046448', delDomain: 'yieldbird-d.openx.net'} },
+        	{ bidder: 'rhythmone', params: { placementId: '205945'} }, /* one placementId for all sizes */
+        	{ bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
+          { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
+          { bidder: 'criteo', params: {networkId: '4902'} }
+        ]
+    },
     {
         code: '/21928950349/banglarpran_300x250',
         mediaTypes: {
@@ -170,7 +170,7 @@ function initAdserver() {
     googletag.cmd.push(function() {
         ubpbjs.que.push(function() {
             ubpbjs.setTargetingForGPTAsync();
-            googletag.pubads().refresh([ub_slot2, ub_slot3]);
+            googletag.pubads().refresh([ub_slot1, ub_slot2, ub_slot3]);
         });
     });
 }
@@ -179,9 +179,9 @@ setTimeout(function() {
     initAdserver();
 }, FAILSAFE_TIMEOUT);
 
-var ub_slot2, ub_slot3;
+var ub_slot1, ub_slot2, ub_slot3;
 googletag.cmd.push(function() {
-    // ub_slot1 = googletag.defineSlot('/21928950349/banglarpran.com_NB_320x50', div_1_sizes, 'div-ub-1').addService(googletag.pubads());
+    ub_slot1 = googletag.defineSlot('/21928950349/banglarpran.com_NB_320x50', div_1_sizes, 'div-ub-1').addService(googletag.pubads());
     ub_slot2 = googletag.defineSlot('/21928950349/banglarpran_300x250', div_2_sizes, 'div-ub-2').addService(googletag.pubads());
     ub_slot3 = googletag.defineSlot('/21928950349/banglarpran_NB_782x90', div_3_sizes, 'div-ub-3').addService(googletag.pubads());
     googletag.pubads().collapseEmptyDivs(true);
@@ -190,17 +190,17 @@ googletag.cmd.push(function() {
     googletag.pubads().enableSingleRequest();
     googletag.enableServices();
     googletag.pubads().addEventListener('slotRenderEnded', function(event) {
-        // if (event.slot === ub_slot1) {
-        //   ub_checkAd1Rendered();
-        // }
-        if (event.slot === ub_slot2) {
+        if (event.slot === ub_slot1) {
+          ub_checkAd1Rendered();
+        }
+        else if (event.slot === ub_slot2) {
           ub_checkAd2Rendered();
         }
         else if (event.slot === ub_slot3) {
           ub_checkAd3Rendered();
         }
-        else if ((event.slot === ub_slot2) && (event.slot === ub_slot3)) {
-          // ub_checkAd1Rendered();
+        else if ((event.slot === ub_slot1) && (event.slot === ub_slot2) && (event.slot === ub_slot3)) {
+          ub_checkAd1Rendered();
           ub_checkAd2Rendered();
           ub_checkAd3Rendered();
           }
@@ -211,7 +211,7 @@ function refreshBid(ub_slot) {
   ubpbjs.que.push(function() {
 	  ubpbjs.requestBids({
 		  timeout: PREBID_TIMEOUT,
-      adUnitCodes: ['/21928950349/banglarpran_300x250', '/21928950349/banglarpran_NB_728x90'],
+       adUnitCodes: ['/21928950349/banglarpran.com_NB_320x50', '/21928950349/banglarpran_300x250', '/21928950349/banglarpran_NB_728x90'],
 		  bidsBackHandler: function() {
         googletag.cmd.push(function() {
           ubpbjs.que.push(function() {
@@ -224,19 +224,19 @@ function refreshBid(ub_slot) {
   });
 }
 
-// ub_ad1RefreshFlag = 0;
-// function ub_checkAd1Rendered(){
-// 	adId1 = 'div-ub-1';
-// 	var nodes = document.getElementById(adId1).childNodes[0].childNodes;
-// 	if(nodes.length && nodes[0].nodeName.toLowerCase() == 'iframe') {
-//     if(ub_ad1RefreshFlag != 1){
-//       setInterval(function() {
-//         ub_ad1RefreshFlag = 1;
-//         refreshBid(ub_slot1);
-//       }, REFRESH_TIMEOUT);
-//     }
-// 	 }
-// }
+ub_ad1RefreshFlag = 0;
+function ub_checkAd1Rendered(){
+	adId1 = 'div-ub-1';
+	var nodes = document.getElementById(adId1).childNodes[0].childNodes;
+	if(nodes.length && nodes[0].nodeName.toLowerCase() == 'iframe') {
+    if(ub_ad1RefreshFlag != 1){
+      setInterval(function() {
+        ub_ad1RefreshFlag = 1;
+        refreshBid(ub_slot1);
+      }, REFRESH_TIMEOUT);
+    }
+	 }
+}
 
 ub_ad2RefreshFlag = 0;
 function ub_checkAd2Rendered(){

30cec662d74a13ed5b61560c892cbd1136638fab
sc0rp1on <singhsooraj.baliyan@gmail.com>
2020-05-29 01:40:16 +0530

    33 Across

--- main/script/adScriptMail.js
+++ main/script/adScriptMail.js
@@ -36,7 +36,7 @@ if(typeof customConfigObjectA === 'undefined'){
           	{ bidder: 'appnexus', params: { placementId: '19307529' } },
             // { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
           	// { bidder: 'oftmedia', params: { placementId: '19196682' } },
-          	// { bidder: '33across', params: { siteId : 'acSmlCBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
+          	{ bidder: '33across', params: { siteId : 'cO7908Orar6OkfaKlId8sQ', productId: 'siab' } },
           	// { bidder: 'emx_digital', params: { tagid: '97458' } }, /* sizeless */
             // { bidder: 'openx', params: {unit: '541046026', delDomain: 'yieldbird-d.openx.net'} },
             { bidder: 'sovrn', params: {tagid: '730845'} },

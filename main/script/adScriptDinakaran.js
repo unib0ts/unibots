e9a5ca2a9f424bf6cdc0e3a3583683a518bf4139
bhu15dr <bhu15dr@gmail.com>
2020-06-04 07:57:06 +0530

    bidders added

--- main/script/adScriptDinakaran.js
+++ main/script/adScriptDinakaran.js
@@ -61,7 +61,7 @@ var adUnits = [
         	// { bidder: '33across', params: { siteId : 'acSmlCBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
         	// { bidder: 'emx_digital', params: { tagid: '97458' } }, /* sizeless */
           { bidder: 'sovrn', params: {tagid: '701550'} },
-          // { bidder: 'openx', params: {unit: '541046026', delDomain: 'yieldbird-d.openx.net'} },
+          { bidder: 'openx', params: {unit: '541046039', delDomain: 'yieldbird-d.openx.net'} },
         	// { bidder: 'rhythmone', params: { placementId: '205372'}}, /* one placementId for all sizes */
         	{ bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
           // { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },

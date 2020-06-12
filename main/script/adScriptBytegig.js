dbfc42b38b011f753377dc4d2558e0cd55b037bf
sc0rp1on <singhsooraj.baliyan@gmail.com>
2020-06-12 16:11:12 +0530

    Old Files to Folder

--- main/script/adScriptBytegig.js
+++ main/script/adScriptBytegig.js
@@ -148,7 +148,7 @@ if (typeof mobileCheck === "function") {
            { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
             // { bidder: 'nobid', params: { siteId : '22027067866'} },
             { bidder: 'criteo', params: {networkId: '4902'} },
-            // { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
+            { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
             // { bidder: 'rubicon', params: {accountId: '11734', siteId: '323604', zoneId: '1680004'} }
           ]
       };
@@ -174,7 +174,7 @@ if (typeof mobileCheck === "function") {
            { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
             // { bidder: 'nobid', params: { siteId : '22027067866'} },
             { bidder: 'criteo', params: {networkId: '4902'} },
-            // { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
+            { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
             // { bidder: 'rubicon', params: {accountId: '11734', siteId: '323604', zoneId: '1680004'} }
           ]
       };
@@ -199,7 +199,7 @@ if (typeof mobileCheck === "function") {
             	{ bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
               // { bidder: 'nobid', params: { siteId : '22027067866'} },
               { bidder: 'criteo', params: {networkId: '4902'} },
-              // { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
+              { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
               // { bidder: 'rubicon', params: {accountId: '11734', siteId: '323604', zoneId: '1680004'} }
             ]
         };
@@ -272,8 +272,20 @@ ubpbjs.que = ubpbjs.que || [];
 ubpbjs.que.push(function() {
     ubpbjs.addAdUnits(adUnits);
     ubpbjs.bidderSettings = {
-      oftmedia: { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-      emx_digital: { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } }
+      'appnexus': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.86; } },
+      'pubmatic': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.74; } },
+      'rubicon': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
+      'openx': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
+      'criteo': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
+      'nobid': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'oftmedia': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.80; } },
+      'sovrn': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.81; } },
+      'adsolut': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+
+      '33across': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'emx_digital': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'rhythmone': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'eplanning': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } }
     };
     ubpbjs.setConfig({
 
@@ -287,8 +299,8 @@ ubpbjs.que.push(function() {
         filterSettings: { iframe: { bidders: [''], filter: 'exclude' }, image:  { bidders: '*', filter: 'include' } },
         // enableOverride: true // publisher will call `ubpbjs.triggerUserSyncs()'
       },
-      debug: true,
-      useBidCache: false,
+      debug: false,
+      useBidCache: true,
       enableSendAllBids: false, // Default will be `true` as of 1.0
       bidderSequence: 'random', // Default is random
       publisherDomain: 'https://bytegig.com/',

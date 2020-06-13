9d7e72499b024fc016a24f7e7cc6d532ee031beb
sc0rp1on <singhsooraj.baliyan@gmail.com>
2020-06-13 17:01:50 +0530

    Madhyamam

--- main/script/adScriptMadhyamam.js
+++ main/script/adScriptMadhyamam.js
@@ -12,7 +12,8 @@ const customConfigObjectA = {
 };
 
 var div_1_sizes = [320, 50];
-
+var div_2_sizes = [[250, 250], [300, 250], [300, 50], [200, 200]];
+var div_3_sizes = [[250, 250], [300, 250], [300, 50], [200, 200]];
 
 var adUnits = [
     {
@@ -72,6 +73,58 @@ var adUnits = [
     }
 ];
 
+if(document.getElementById('HomePage_300x250_1') != null){
+  var adUnits1 = {
+      code: '/21957769615/madhyamam.com_wm_300x250_1',
+      mediaTypes: {
+          banner: {
+              sizes: div_2_sizes
+          }
+      },
+      bids: [
+        { bidder: 'appnexus', params: { placementId: '19056048' } }, /* one placementId for all sizes  my appnexus bidder */
+        { bidder: 'oftmedia', params: { placementId: '18677464' } },
+        { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
+        { bidder: 'openx', params: {unit: '541046178', delDomain: 'yieldbird-d.openx.net'} },
+        { bidder: '33across', params: { siteId : 'a8_MFsBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
+        { bidder: 'emx_digital', params: { tagid: '97490' } }, /* sizeless */
+        { bidder: 'sovrn', params: {tagid: '738357'} },
+        { bidder: 'rhythmone', params: { placementId: '205372'}}, /* one placementId for all sizes */
+        { bidder: 'nobid', params: { siteId : '21986889043'} },
+        { bidder: 'criteo', params: {networkId: '4902'} },
+        // { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
+        { bidder: 'rubicon', params: {accountId: '11734', siteId: '323748', zoneId: '1680438'} }
+      ]
+  }
+  adUnits.push(adUnits1);
+}
+if(document.getElementById('div-gpt-ad-1590738750383-0') != null){
+
+  adUnits1 = {
+      code: '/21957769615/madhyamam.com_wm_300x250_2',
+      mediaTypes: {
+          banner: {
+              sizes: div_3_sizes
+          }
+      },
+      bids: [
+        { bidder: 'appnexus', params: { placementId: '19056048' } }, /* one placementId for all sizes  my appnexus bidder */
+        { bidder: 'oftmedia', params: { placementId: '18677464' } },
+        { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
+        { bidder: 'openx', params: {unit: '541046178', delDomain: 'yieldbird-d.openx.net'} },
+        { bidder: '33across', params: { siteId : 'a8_MFsBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
+        { bidder: 'emx_digital', params: { tagid: '97490' } }, /* sizeless */
+        { bidder: 'sovrn', params: {tagid: '738358'} },
+        { bidder: 'rhythmone', params: { placementId: '205372'}}, /* one placementId for all sizes */
+        { bidder: 'nobid', params: { siteId : '21986889043'} },
+        { bidder: 'criteo', params: {networkId: '4902'} },
+        // { bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
+        { bidder: 'rubicon', params: {accountId: '11734', siteId: '323748', zoneId: '1680438'} }
+      ]
+  }
+  adUnits.push(adUnits1);
+}
+
 // ======== DO NOT EDIT BELOW THIS LINE =========== //
 var googletag = googletag || {};
 googletag.cmd = googletag.cmd || [];
@@ -127,63 +180,133 @@ ubpbjs.que.push(function() {
     });
 });
 
+var mappings = {
+  slots: [],
+  adCode: [],
+  slotNumbers: [],
+  sizes: [],
+  adId: [],
+  renderedFlag: []
+};
+
+var wm_mappings = {
+  targetUnits: ['HomePage_300x250_1', 'div-gpt-ad-1590738750383-0'],
+  slotNames: ['ub_slot2', 'ub_slot3'],
+  adUnitNames: ['/21957769615/madhyamam.com_wm_300x250_1', '/21957769615/madhyamam.com_wm_300x250_2'],
+  adId: ['div-ub-wm-2', 'div-ub-wm-3'],
+  sizes: [div_2_sizes, div_3_sizes]
+}
+
+function ub_checkAdRendered(adId, ub_slot, adCode){
+  ub_slotNum = ub_slot[ub_slot.length-1];
+  if(typeof mappings.renderedFlag[ub_slotNum] || !mappings.renderedFlag[ub_slotNum]){
+    adId1 = adId;
+    var nodes = document.getElementById(adId1).childNodes[0].childNodes;
+    if(nodes.length && nodes[0].nodeName.toLowerCase() == 'iframe') {
+      setTimeout(function() {
+        refreshBid(ub_slot, adCode);
+      }, REFRESH_TIMEOUT);
+      mappings.renderedFlag[ub_slotNum] = true;
+    }
+  }
+}
+
+function refreshBid(ub_slot, adCode) {
+  ubpbjs.que.push(function(){
+    ubpbjs.requestBids({
+      timeout: PREBID_TIMEOUT,
+      adUnitCodes: adCode,
+      bidsBackHandler: function() {
+        googletag.cmd.push(function() {
+          ubpbjs.que.push(function() {
+              ubpbjs.setTargetingForGPTAsync();
+              googletag.pubads().refresh(ub_slot);
+          });
+        });
+      }
+    });
+  });
+}
+
 function initAdserver() {
     if (ubpbjs.initAdserverSet) return;
     ubpbjs.initAdserverSet = true;
     googletag.cmd.push(function() {
         ubpbjs.que.push(function() {
             ubpbjs.setTargetingForGPTAsync();
-            googletag.pubads().refresh([ub_slot1]);
+            googletag.pubads().refresh(mappings.slots);
         });
     });
 }
-// in case ubpbjs doesn't load
-setTimeout(function() {
-    initAdserver();
-}, FAILSAFE_TIMEOUT);
 
-var ub_slot1;
-googletag.cmd.push(function() {
-    ub_slot1 = googletag.defineSlot('/21957769615/madhyamam.com_nb_320x50', div_1_sizes, 'div-ub-1').addService(googletag.pubads());
+function googleDefine(slotNumbers, adCode, sizes, adId){
+  for(var i=0; i<slotNumbers.length;i++){
+    eval('ub_slot'+slotNumbers[i]+ '= '+'googletag.defineSlot(adCode[i], sizes[i], adId[i])');
+    var a = eval('ub_slot'+slotNumbers[i]);
+    a.addService(googletag.pubads());
+    mappings.slots.push(eval('ub_slot'+slotNumbers[i]));
+  }
+}
+
+function googlePush(){
+  googletag.cmd.push(function() {
     googletag.pubads().collapseEmptyDivs(true);
     googletag.pubads().setCentering(true);
     googletag.pubads().setPrivacySettings({ 'restrictDataProcessing': true });
     googletag.pubads().enableSingleRequest();
     googletag.enableServices();
-    googletag.pubads().addEventListener('slotRenderEnded', function(event) {
-        if (event.slot === ub_slot1) {
-          ub_checkAdRendered();
-        }
-    });
-});
-
-function refreshBid() {
-  ubpbjs.que.push(function() {
-	  ubpbjs.requestBids({
-		  timeout: PREBID_TIMEOUT,
-		  adUnitCodes: ['/21957769615/madhyamam.com_nb_320x50'],
-		  bidsBackHandler: function() {
-        googletag.cmd.push(function() {
-          ubpbjs.que.push(function() {
-              ubpbjs.setTargetingForGPTAsync();
-              googletag.pubads().refresh([ub_slot1]);
-          });
-        });
-		  }
-	  });
   });
 }
 
-ub_adRefreshFlag = 0;
-function ub_checkAdRendered(){
-	adId = 'div-ub-1';
-	var nodes = document.getElementById(adId).childNodes[0].childNodes;
-	if(nodes.length && nodes[0].nodeName.toLowerCase() == 'iframe') {
-    if(ub_adRefreshFlag != 1){
-      setInterval(function() {
-        ub_adRefreshFlag = 1;
-        refreshBid();
-      }, REFRESH_TIMEOUT);
-    }
-	 }
+if (document.getElementById('div-ub-1')){
+  mappings.slotNumbers.push(1);
+  mappings.adCode.push('/21957769615/madhyamam.com_nb_320x50');
+  mappings.sizes.push(div_1_sizes);
+  mappings.adId.push('div-ub-1');
+  googletag.cmd.push(function() {
+    googletag.pubads().addEventListener('slotRenderEnded', function(event) {
+      if (event.slot === ub_slot1) {
+        ub_checkAdRendered('div-ub-1', ub_slot1, ['/21957769615/madhyamam.com_nb_320x50']);
+      }
+    });
+  });
 }
+for(var i=0; i<wm_mappings.targetUnits.length; i++){
+  if (document.getElementById(wm_mappings.targetUnits[i]) != null) {
+    targetUnit = document.getElementById(wm_mappings.targetUnits[i]);
+    parent = targetUnit.parentNode;
+    parent.innerHTML = '';
+    var z= document.createElement('div');
+    z.id = wm_mappings.adId[i];
+    z.className = wm_mappings.adId[i];
+    parent.appendChild(z);
+
+    mappings.slotNumbers.push(i+2);
+    mappings.adCode.push(wm_mappings.adUnitNames[i]);
+    mappings.sizes.push(wm_mappings.sizes[i]);
+    mappings.adId.push(wm_mappings.adId[i]);
+    googletag.cmd.push(function() {
+      googletag.pubads().addEventListener('slotRenderEnded', function(event) {
+        if (event.slot === wm_mappings.slotNames[i]) {
+          ub_checkAdRendered(wm_mappings.adId[i], wm_mappings.slotNames[i], [wm_mappings.adUnitNames[i]]);
+        }
+      });
+    });
+  }
+}
+
+
+if(typeof googletag.defineSlot === "function"){
+  googleDefine(mappings.slotNumbers, mappings.adCode, mappings.sizes, mappings.adId);
+  googlePush();
+}
+else{
+  setTimeout(function(){
+    googleDefine(mappings.slotNumbers, mappings.adCode, mappings.sizes, mappings.adId);
+    googlePush();
+  }, 500);
+}
+
+setTimeout(function() {
+    initAdserver();
+}, FAILSAFE_TIMEOUT);

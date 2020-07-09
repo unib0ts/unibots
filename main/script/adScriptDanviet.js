20b0bf10bdfe6db5816a50d39eadd64b397c5c26
sc0rp1on <singhsooraj.baliyan@gmail.com>
2020-07-10 01:44:47 +0530

    refresh fixed

--- main/script/adScriptDanviet.js
+++ main/script/adScriptDanviet.js
@@ -191,7 +191,7 @@ function refreshBid(ub_slot, adCode) {
         googletag.cmd.push(function() {
           ubpbjs.que.push(function() {
               ubpbjs.setTargetingForGPTAsync();
-              googletag.pubads().refresh(ub_slot);
+              googletag.pubads().refresh([ub_slot]);
           });
         });
       }

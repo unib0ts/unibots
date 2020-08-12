b0e9260a6a6d2cb48add75d70e4b4d714071e05d
bhu15dr <bhu15dr@gmail.com>
2020-08-12 16:05:05 +0530

    bidders

--- main/script/adScriptWorkbank.js
+++ main/script/adScriptWorkbank.js
@@ -144,7 +144,7 @@ if (typeof mobileCheck === "function") {
             }
         },
         bids: [
-          // { bidder: 'appnexus', params: { placementId: '19455531' } }, /* one placementId for all sizes  my appnexus bidder */
+          { bidder: 'appnexus', params: { placementId: '19928396' } }, /* one placementId for all sizes  my appnexus bidder */
           // { bidder: 'sovrn', params: {tagid: '736637'} },
           // { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '2952064'} },
          // { bidder: 'oftmedia', params: { placementId: '18671523' } },
@@ -175,7 +175,7 @@ if (typeof mobileCheck === "function") {
             }
         },
         bids: [
-         //    { bidder: 'appnexus', params: { placementId: '19455531' } }, /* one placementId for all sizes  my appnexus bidder */
+            { bidder: 'appnexus', params: { placementId: '19928396' } }, /* one placementId for all sizes  my appnexus bidder */
          // // { bidder: 'oftmedia', params: { placementId: '18671523' } },
          //    { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '2952065'} },
          // // { bidder: '33across', params: { siteId : 'acSmlCBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
@@ -206,7 +206,7 @@ if (typeof mobileCheck === "function") {
             }
         },
         bids: [
-         //    { bidder: 'appnexus', params: { placementId: '19455531' } }, /* one placementId for all sizes  my appnexus bidder */
+            { bidder: 'appnexus', params: { placementId: '19928396' } }, /* one placementId for all sizes  my appnexus bidder */
          // // // { bidder: 'oftmedia', params: { placementId: '18671523' } },
          //  { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '2952068'} },
          // // { bidder: '33across', params: { siteId : 'acSmlCBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/

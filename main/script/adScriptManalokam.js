5849ab30e5ac03a0d1ad5a6decc3305c60bb2b7e
bhu15dr <bhu15dr@gmail.com>
2020-08-14 14:44:06 +0530

    bidders

--- main/script/adScriptManalokam.js
+++ main/script/adScriptManalokam.js
@@ -129,7 +129,7 @@ const customConfigObjectA = {
        { bidder: 'sovrn', params: {tagid: '759850'} },
        // { bidder: 'smartadserver', params: { siteId: '362166', pageId: '1289666', formatId: '93231', domain: 'https://prg8.smartadserver.com' } },
        // //{ bidder: 'sonobi', params: { placement_id: 'e061c85c1bf277a0a913', ad_unit: 'ragalahari_NB_728x90' } },
-       // { bidder: 'onetag', params: { pubId: '60c32c42465aac2' } },
+       { bidder: 'onetag', params: { pubId: '60c32c42465aac2' } },
        // { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '3099856'} },
        // { bidder: 'criteo', params: {networkId: '10542'} },
        // { bidder: 'criteointl', params: {networkId: '10545'} },

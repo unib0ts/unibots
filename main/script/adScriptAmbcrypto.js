a8e4376acfdcf8acc65d2fb9542038dc7d3c5548
bhu15dr <bhu15dr@gmail.com>
2020-08-24 09:39:15 +0530

    biddrs

--- main/script/adScriptAmbcrypto.js
+++ main/script/adScriptAmbcrypto.js
@@ -76,7 +76,7 @@ var hb_full_common_bidders = [
   // { bidder: '33across', params: { siteId : 'bPSPrGBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
   // { bidder: 'emx_digital', params: { tagid: '97515' } }, /* sizeless */
   // { bidder: 'rhythmone', params: { placementId: '205945'} }, /* one placementId for all sizes */
-  // { bidder: 'nobid', params: { siteId: '21987177881' } },
+  { bidder: 'nobid', params: { siteId: '22088461846' } },
   // { bidder: 'openx', params: {unit: '541046310', delDomain: 'yieldbird-d.openx.net'} },
   // { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
   // //{ bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },

d0b200e1aa33135cf91ac4fefae18a245650c175
bhu15dr <bhu15dr@gmail.com>
2020-09-16 16:31:37 +0530

    adcode chng

--- main/script/adScriptDML.js
+++ main/script/adScriptDML.js
@@ -2,33 +2,33 @@ var PREBID_TIMEOUT = 2000;
 var FAILSAFE_TIMEOUT = 3000;
 var REFRESH_TIMEOUT = 60000;
 
-var GEO_CODE = '';
-(function (){
-  var request = new XMLHttpRequest();
-		url = 'https://pro.ip-api.com/json/?fields=status,message,countryCode&key=LWKtz4EzQwMJRyQ';
-		request.open('GET', url, true);
-		request.onload = function() {
-			if (request.status >= 200 && request.status < 400) {
-				var data = request.responseText;
-				data = JSON.parse(data);
-				if(data.status == "success") {
-          GEO_CODE = data.countryCode;
-				}
-				else {
-					console.error("Geo Request Failed");
-				}
-			}
-			else {
-				console.error('Request failed from server');
-			}
-      mainHbRun();
-		};
-		request.onerror = function() {
-			console.error('Request failed to Reach GEO Server');
-      mainHbRun();
-		};
-		request.send();
-})();
+// var GEO_CODE = '';
+// (function (){
+//   var request = new XMLHttpRequest();
+// 		url = 'https://pro.ip-api.com/json/?fields=status,message,countryCode&key=LWKtz4EzQwMJRyQ';
+// 		request.open('GET', url, true);
+// 		request.onload = function() {
+// 			if (request.status >= 200 && request.status < 400) {
+// 				var data = request.responseText;
+// 				data = JSON.parse(data);
+// 				if(data.status == "success") {
+//           GEO_CODE = data.countryCode;
+// 				}
+// 				else {
+// 					console.error("Geo Request Failed");
+// 				}
+// 			}
+// 			else {
+// 				console.error('Request failed from server');
+// 			}
+//       mainHbRun();
+// 		};
+// 		request.onerror = function() {
+// 			console.error('Request failed to Reach GEO Server');
+//       mainHbRun();
+// 		};
+// 		request.send();
+// })();
 
 const customConfigObjectA = {
  "buckets" : [{
@@ -43,41 +43,41 @@ var div_1_sizes = [320, 50];
 
 
 var adUnits = [
-     {
-         code: '/1028307/Dinamalar_Desktop_Unibot_Adslot_320x50',
-         mediaTypes: {
-           native: {
-             image: {
-               // sizes: [300, 100],
-               aspect_ratios: [{
-                 min_width: 100,        /* Optional */
-                 min_height: 50,       /* Optional */
-                 ratio_width: 2,        /* Required */
-                 ratio_height: 1,       /* Required */
-               }],
-               sendId: true
-             },
-             title: {
-               len: 50,
-               sendId: true
-             },
-             sponsoredBy: {
-               required: true,
-               sendId: true
-             },
-             clickUrl: {
-               required: true,
-               sendId: true
-             }
-           }
-         },
-         bids: [
-           { bidder: 'appnexus', params: { placementId: '19057738' } },
-           { bidder: 'oftmedia', params: { placementId: '19094359' } }
-         ]
-     },
+     // {
+     //     code: '/21956916242/dinamalar.com_nb_320x50',
+     //     mediaTypes: {
+     //       native: {
+     //         image: {
+     //           // sizes: [300, 100],
+     //           aspect_ratios: [{
+     //             min_width: 100,        /* Optional */
+     //             min_height: 50,       /* Optional */
+     //             ratio_width: 2,        /* Required */
+     //             ratio_height: 1,       /* Required */
+     //           }],
+     //           sendId: true
+     //         },
+     //         title: {
+     //           len: 50,
+     //           sendId: true
+     //         },
+     //         sponsoredBy: {
+     //           required: true,
+     //           sendId: true
+     //         },
+     //         clickUrl: {
+     //           required: true,
+     //           sendId: true
+     //         }
+     //       }
+     //     },
+     //     bids: [
+     //       { bidder: 'appnexus', params: { placementId: '19057738' } },
+     //       { bidder: 'oftmedia', params: { placementId: '19094359' } }
+     //     ]
+     // },
     {
-        code: '/1028307/Dinamalar_Desktop_Unibot_Adslot_320x50',
+        code: '/21956916242/dinamalar.com_nb_320x50',
         mediaTypes: {
             banner: {
                 sizes: div_1_sizes
@@ -85,21 +85,21 @@ var adUnits = [
         },
         bids: [
         	{ bidder: 'appnexus', params: { placementId: '19057738' } }, /* one placementId for all sizes  my appnexus bidder */
-        	{ bidder: 'oftmedia', params: { placementId: '19094359' } },
-          { bidder: '33across', params: { siteId : 'bhgbWQWuGr6PjyaKlId8sQ', productId: 'siab' }, labelAll: ["US"] }, /*All sizes*/
+        	// { bidder: 'oftmedia', params: { placementId: '19094359' } },
+          // { bidder: '33across', params: { siteId : 'bhgbWQWuGr6PjyaKlId8sQ', productId: 'siab' }, labelAll: ["US"] }, /*All sizes*/
         	// //{ bidder: 'emx_digital', params: { tagid: '97450' } }, /* sizeless */
-          { bidder: 'sovrn', params: {tagid: '716626'} },
+          // { bidder: 'sovrn', params: {tagid: '716626'} },
           // { bidder: 'openx', params: {unit: '541046401', delDomain: 'yieldbird-d.openx.net'} },
-        	{ bidder: 'rhythmone', params: { placementId: '205945'} }, /* one placementId for all sizes */
+        	// { bidder: 'rhythmone', params: { placementId: '205945'} }, /* one placementId for all sizes */
         	// { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
-          { bidder: 'nobid', params: { siteId : '22049999662'} },
+          // { bidder: 'nobid', params: { siteId : '22049999662'} },
           //{ bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
-          { bidder: 'criteo', params: {networkId: '10542'} },
-          { bidder: 'smartadserver', params: { siteId: '362109', pageId: '1289610', formatId: '93231', domain: 'https://prg8.smartadserver.com' } },
+          // { bidder: 'criteo', params: {networkId: '10542'} },
+          // { bidder: 'smartadserver', params: { siteId: '362109', pageId: '1289610', formatId: '93231', domain: 'https://prg8.smartadserver.com' } },
           //{ bidder: 'sonobi', params: { placement_id: 'e061c85c1bf277a0a913', ad_unit: 'ragalahari_NB_728x90' } },
-          { bidder: 'onetag', params: { pubId: '60c32c42465aac2' } },
-          { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '2996029'} },
-          { bidder: 'criteointl', params: {networkId: '10545'} }
+          // { bidder: 'onetag', params: { pubId: '60c32c42465aac2' } },
+          // { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '2996029'} },
+          // { bidder: 'criteointl', params: {networkId: '10545'} }
         ]
     }
 ];
@@ -114,6 +114,77 @@ googletag.cmd.push(function() {
 var ubpbjs = ubpbjs || {};
 ubpbjs.que = ubpbjs.que || [];
 
+ubpbjs.que.push(function() {
+    ubpbjs.addAdUnits(adUnits);
+    ubpbjs.aliasBidder('criteo','criteointl');
+    ubpbjs.bidderSettings = {
+      'appnexus': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.86; } },
+      'pubmatic': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.74; } },
+      'rubicon': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
+      'openx': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
+      'criteo': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+      'criteointl': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+      'nobid': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'oftmedia': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.80; } },
+      'sovrn': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.81; } },
+      //'adsolut': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'onetag': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+      // 'sonobi': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+      // 'smartadserver': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+
+      '33across': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'emx_digital': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'rhythmone': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'eplanning': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } }
+    };
+    ubpbjs.setConfig({
+
+      priceGranularity: customConfigObjectA,
+     //consentManagement: { gdpr: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*400, allowAuctionWithoutConsent: true }, usp: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*400 } },
+      //cache: {url: "https://prebid.adnxs.com/pbc/v1/cache"},
+      userSync: {
+          iframeEnabled: true,
+          syncsPerBidder: 999, // and no more than 3 syncs at a time
+          // syncDelay: PREBID_TIMEOUT*4, // 5 seconds after the auction
+          filterSettings: { iframe: { bidders: [''], filter: 'exclude' }, image:  { bidders: '*', filter: 'include' } },
+          // enableOverride: true // publisher will call `ubpbjs.triggerUserSyncs()'
+          userIds: [{
+              name: "id5Id",
+              params: {
+                  partner: 438,            // change to the Partner Number you received from ID5
+              },
+              storage: {
+                  type: "cookie",
+                  name: "id5id.1st",       // create a cookie with this name
+                  expires: 90,             // cookie lasts for 90 days
+                  refreshInSeconds: 8*3600 // refresh ID every 8 hours to ensure it is fresh
+              }
+          }],
+          auctionDelay: 500},
+      debug: false,
+      useBidCache: true,
+      enableSendAllBids: false, // Default will be `true` as of 1.0
+      bidderSequence: 'random', // Default is random
+      publisherDomain: 'dinamalar.com',
+      bidderTimeout: PREBID_TIMEOUT+500,
+      //pubcid: {expInterval: },
+      //currency: { 'adServerCurrency': "GBP", 'granularityMultiplier': 1, 'conversionRateFile': 'https://cdn.jsdelivr.net/gh/prebid/currency-file@1/latest.json', },
+    "currency": {
+       // enables currency feature
+       "adServerCurrency": "AED",
+       "granularityMultiplier":3 ,
+       // optionally override the default rate file
+       "conversionRateFile": "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/currency/currency.json",
+       // optionally provide a default rate in case the file can't be read
+       "defaultRates": { "USD": { "AED": 3.67 }}
+     }
+    });
+    ubpbjs.requestBids({
+      bidsBackHandler: initAdserver,
+      timeout: PREBID_TIMEOUT,
+    });
+});
+
 function initAdserver() {
     if (ubpbjs.initAdserverSet) return;
     ubpbjs.initAdserverSet = true;
@@ -125,9 +196,14 @@ function initAdserver() {
     });
 }
 
+// in case ubpbjs doesn't load
+setTimeout(function() {
+    initAdserver();
+}, FAILSAFE_TIMEOUT);
+
 var ub_slot1;
 googletag.cmd.push(function() {
-    ub_slot1 = googletag.defineSlot('/1028307/Dinamalar_Desktop_Unibot_Adslot_320x50', div_1_sizes, 'div-ub-1').addService(googletag.pubads());
+    ub_slot1 = googletag.defineSlot('/21956916242/dinamalar.com_nb_320x50', div_1_sizes, 'div-gpt-ad-1600244730786-0').addService(googletag.pubads());
     googletag.pubads().collapseEmptyDivs(true);
     googletag.pubads().setCentering(true);
     googletag.pubads().setPrivacySettings({ 'restrictDataProcessing': true });
@@ -144,7 +220,7 @@ function refreshBid() {
   ubpbjs.que.push(function() {
 	  ubpbjs.requestBids({
 		  timeout: PREBID_TIMEOUT,
-		  adUnitCodes: ['/1028307/Dinamalar_Desktop_Unibot_Adslot_320x50'],
+		  adUnitCodes: ['/21956916242/dinamalar.com_nb_320x50'],
 		  bidsBackHandler: function() {
         googletag.cmd.push(function() {
           ubpbjs.que.push(function() {
@@ -159,7 +235,7 @@ function refreshBid() {
 
 ub_adRefreshFlag = 0;
 function ub_checkAdRendered(){
-	adId = 'div-ub-1';
+	adId = 'div-gpt-ad-1600244730786-0';
 	var nodes = document.getElementById(adId).childNodes[0].childNodes;
 	if(nodes.length && nodes[0].nodeName.toLowerCase() == 'iframe') {
     if(ub_adRefreshFlag != 1){
@@ -169,82 +245,4 @@ function ub_checkAdRendered(){
       }, REFRESH_TIMEOUT);
     }
 	 }
-}
-
-function mainHbRun(){
-  ubpbjs.que.push(function() {
-      ubpbjs.addAdUnits(adUnits);
-      ubpbjs.aliasBidder('criteo','criteointl');
-      ubpbjs.bidderSettings = {
-        'appnexus': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.86; } },
-        'pubmatic': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.74; } },
-        'rubicon': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
-        'openx': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
-        'criteo': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-        'criteointl': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-        'nobid': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'oftmedia': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.80; } },
-        'sovrn': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.81; } },
-        //'adsolut': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'onetag': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-        // 'sonobi': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-        // 'smartadserver': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-
-        '33across': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'emx_digital': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'rhythmone': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'eplanning': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } }
-      };
-      ubpbjs.setConfig({
-
-      	priceGranularity: customConfigObjectA,
-       //consentManagement: { gdpr: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*400, allowAuctionWithoutConsent: true }, usp: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*400 } },
-        //cache: {url: "https://prebid.adnxs.com/pbc/v1/cache"},
-        userSync: {
-            iframeEnabled: true,
-            syncsPerBidder: 999, // and no more than 3 syncs at a time
-            // syncDelay: PREBID_TIMEOUT*4, // 5 seconds after the auction
-            filterSettings: { iframe: { bidders: [''], filter: 'exclude' }, image:  { bidders: '*', filter: 'include' } },
-            // enableOverride: true // publisher will call `ubpbjs.triggerUserSyncs()'
-            userIds: [{
-                name: "id5Id",
-                params: {
-                    partner: 438,            // change to the Partner Number you received from ID5
-                },
-                storage: {
-                    type: "cookie",
-                    name: "id5id.1st",       // create a cookie with this name
-                    expires: 90,             // cookie lasts for 90 days
-                    refreshInSeconds: 8*3600 // refresh ID every 8 hours to ensure it is fresh
-                }
-            }],
-            auctionDelay: 500},
-        debug: false,
-        useBidCache: true,
-        enableSendAllBids: false, // Default will be `true` as of 1.0
-        bidderSequence: 'random', // Default is random
-        publisherDomain: 'dinamalar.com',
-        bidderTimeout: PREBID_TIMEOUT+500,
-        //pubcid: {expInterval: },
-        //currency: { 'adServerCurrency': "GBP", 'granularityMultiplier': 1, 'conversionRateFile': 'https://cdn.jsdelivr.net/gh/prebid/currency-file@1/latest.json', },
-      "currency": {
-         // enables currency feature
-         "adServerCurrency": "AED",
-         "granularityMultiplier":3 ,
-         // optionally override the default rate file
-         "conversionRateFile": "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/currency/currency.json",
-         // optionally provide a default rate in case the file can't be read
-         "defaultRates": { "USD": { "AED": 3.67 }}
-       }
-      });
-      ubpbjs.requestBids({
-        bidsBackHandler: initAdserver,
-        timeout: PREBID_TIMEOUT,
-        labels: [GEO_CODE],
-      });
-  });
-  // in case ubpbjs doesn't load
-  setTimeout(function() {
-      initAdserver();
-  }, FAILSAFE_TIMEOUT);
 }

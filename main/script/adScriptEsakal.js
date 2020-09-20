bdd509ad5190fae62956d992db5fa480e5d372ab
bhu15dr <bhu15dr@gmail.com>
2020-09-20 18:01:44 +0530

    adcode

--- main/script/adScriptEsakal.js
+++ main/script/adScriptEsakal.js
@@ -8,34 +8,6 @@ var PREBID_TIMEOUT = 2000;
 var FAILSAFE_TIMEOUT = 3000;
 var REFRESH_TIMEOUT = 60000;
 
-var GEO_CODE = '';
-(function (){
-  var request = new XMLHttpRequest();
-		url = 'https://pro.ip-api.com/json/?fields=status,message,countryCode&key=LWKtz4EzQwMJRyQ';
-		request.open('GET', url, true);
-		request.onload = function() {
-			if (request.status >= 200 && request.status < 400) {
-				var data = request.responseText;
-				data = JSON.parse(data);
-				if(data.status == "success") {
-          GEO_CODE = data.countryCode;
-				}
-				else {
-					console.error("Geo Request Failed");
-				}
-			}
-			else {
-				console.error('Request failed from server');
-			}
-      mainHbRun();
-		};
-		request.onerror = function() {
-			console.error('Request failed to Reach GEO Server');
-      mainHbRun();
-		};
-		request.send();
-})();
-
 const customConfigObjectA = {
  "buckets" : [{
     "precision": 2,  //default is 2 if omitted - means 2.1234 rounded to 2 decimal places = 2.12
@@ -146,7 +118,7 @@ const customConfigObjectA = {
   //   }
   adUnits1 =
   {
-      code: '/21928950349/andhrajyothy.com_NB_320x50',
+      code: '/115394472/Esakal_320x50_Sticky',
       mediaTypes: {
           banner: {
               sizes: div_1_sizes
@@ -162,8 +134,8 @@ const customConfigObjectA = {
        // // { bidder: 'criteo', params: {networkId: '10542'} },
        // // { bidder: 'criteointl', params: {networkId: '10545'} },
        // // // { bidder: 'oftmedia', params: { placementId: '18671523' } },
-       { bidder: '33across', params: { siteId : 'd0h6GkBt8r6PWLaKlId8sQ', productId: 'siab' }, labelAll: ["US"] }, /*All sizes*/
-       { bidder: 'emx_digital', params: { tagid: '97452' } }, /* sizeless */
+       // // { bidder: '33across', params: { siteId : 'c879m0WuGr6PjyaKlId8sQ', productId: 'siab' } }, /*All sizes*/
+       // // // { bidder: 'emx_digital', params: { tagid: '97458' } }, /* sizeless */
        // //  // { bidder: 'openx', params: {unit: '541046026', delDomain: 'yieldbird-d.openx.net'} },
        // // // { bidder: 'rhythmone', params: { placementId: '211538'}}, /* one placementId for all sizes */
        // // // { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
@@ -185,6 +157,76 @@ googletag.cmd.push(function() {
 
 var ubpbjs = ubpbjs || {};
 ubpbjs.que = ubpbjs.que || [];
+
+ubpbjs.que.push(function() {
+    ubpbjs.addAdUnits(adUnits);
+    ubpbjs.aliasBidder('criteo','criteointl');
+    ubpbjs.bidderSettings = {
+      'appnexus': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.86; } },
+      'pubmatic': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.74; } },
+      'rubicon': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
+      'openx': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
+      'criteo': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+      'criteointl': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+      'nobid': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'oftmedia': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.80; } },
+      'sovrn': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.81; } },
+      //'adsolut': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'onetag': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+      // 'sonobi': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+      // 'smartadserver': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+
+      '33across': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'emx_digital': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'rhythmone': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+      'eplanning': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } }
+    };
+    ubpbjs.setConfig({
+
+    	priceGranularity: customConfigObjectA,
+     //consentManagement: { gdpr: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*400, allowAuctionWithoutConsent: true }, usp: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*400 } },
+      //cache: {url: "https://prebid.adnxs.com/pbc/v1/cache"},
+      userSync: {
+          iframeEnabled: true,
+          syncsPerBidder: 999, // and no more than 3 syncs at a time
+          // syncDelay: PREBID_TIMEOUT*4, // 5 seconds after the auction
+          filterSettings: { iframe: { bidders: [''], filter: 'exclude' }, image:  { bidders: '*', filter: 'include' } },
+          // enableOverride: true // publisher will call `ubpbjs.triggerUserSyncs()'
+          userIds: [{
+              name: "id5Id",
+              params: {
+                  partner: 438,            // change to the Partner Number you received from ID5
+              },
+              storage: {
+                  type: "cookie",
+                  name: "id5id.1st",       // create a cookie with this name
+                  expires: 90,             // cookie lasts for 90 days
+                  refreshInSeconds: 8*3600 // refresh ID every 8 hours to ensure it is fresh
+              }
+          }],
+          auctionDelay: 500},
+      debug: false,
+      useBidCache: true,
+      enableSendAllBids: false, // Default will be `true` as of 1.0
+      bidderSequence: 'random', // Default is random
+      publisherDomain: 'https://www.esakal.com/',
+      bidderTimeout: PREBID_TIMEOUT+500,
+      //pubcid: {expInterval: },
+      // "currency": {
+      //    // enables currency feature
+      //    "adServerCurrency": "AED",
+      //    "granularityMultiplier":3 ,
+      //    // optionally override the default rate file
+      //    "conversionRateFile": "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/currency/currency.json",
+      //    // optionally provide a default rate in case the file can't be read
+      //    "defaultRates": { "USD": { "AED": 3.67 }}
+      //  }
+     });
+    ubpbjs.requestBids({
+        bidsBackHandler: initAdserver,
+        timeout: PREBID_TIMEOUT
+    });
+});
 
 var mappings = {
   slots: [],
@@ -299,13 +341,13 @@ function googlePush(){
   //   }
   // }
   mappings.slotNumbers.push(1);
-  mappings.adCode.push('/21928950349/andhrajyothy.com_NB_320x50');
+  mappings.adCode.push('/115394472/Esakal_320x50_Sticky');
   mappings.sizes.push(div_1_sizes);
-  mappings.adId.push('div-gpt-ad-ub-1');
+  mappings.adId.push('div-gpt-ad-1600341796839-0');
   googletag.cmd.push(function() {
     googletag.pubads().addEventListener('slotRenderEnded', function(event) {
       if (event.slot === ub_slot1) {
-        ub_checkAdRendered('div-gpt-ad-ub-1', ub_slot1, ['/21928950349/andhrajyothy.com_NB_320x50']);
+        ub_checkAdRendered('div-gpt-ad-1600341796839-0', ub_slot1, ['/115394472/Esakal_320x50_Sticky']);
       }
     });
   });
@@ -321,71 +363,6 @@ else{
     googlePush();
   }, 500);
 }
-function mainHbRun(){
-  ubpbjs.que.push(function() {
-      ubpbjs.addAdUnits(adUnits);
-      ubpbjs.aliasBidder('criteo','criteointl');
-      ubpbjs.bidderSettings = {
-        'appnexus': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.86; } },
-        'pubmatic': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.74; } },
-        'rubicon': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
-        'openx': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
-        'criteo': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-        'criteointl': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-        'nobid': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'oftmedia': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.80; } },
-        'sovrn': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.81; } },
-        //'adsolut': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'onetag': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-        // 'sonobi': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-        // 'smartadserver': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-
-        '33across': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'emx_digital': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'rhythmone': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-        'eplanning': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } }
-      };
-      ubpbjs.setConfig({
-
-      	priceGranularity: customConfigObjectA,
-       //consentManagement: { gdpr: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*400, allowAuctionWithoutConsent: true }, usp: { cmpApi: 'iab', timeout: PREBID_TIMEOUT*400 } },
-        //cache: {url: "https://prebid.adnxs.com/pbc/v1/cache"},
-        userSync: {
-            iframeEnabled: true,
-            syncsPerBidder: 999, // and no more than 3 syncs at a time
-            // syncDelay: PREBID_TIMEOUT*4, // 5 seconds after the auction
-            filterSettings: { iframe: { bidders: [''], filter: 'exclude' }, image:  { bidders: '*', filter: 'include' } },
-            // enableOverride: true // publisher will call `ubpbjs.triggerUserSyncs()'
-            userIds: [{
-                name: "id5Id",
-                params: {
-                    partner: 438,            // change to the Partner Number you received from ID5
-                },
-                storage: {
-                    type: "cookie",
-                    name: "id5id.1st",       // create a cookie with this name
-                    expires: 90,             // cookie lasts for 90 days
-                    refreshInSeconds: 8*3600 // refresh ID every 8 hours to ensure it is fresh
-                }
-            }],
-            auctionDelay: 500},
-        debug: false,
-        useBidCache: true,
-        enableSendAllBids: false, // Default will be `true` as of 1.0
-        bidderSequence: 'random', // Default is random
-        publisherDomain: 'https://www.andhrajyothy.com/',
-        bidderTimeout: PREBID_TIMEOUT+500,
-        //pubcid: {expInterval: },
-        //currency: { 'adServerCurrency': "GBP", 'granularityMultiplier': 1, 'conversionRateFile': 'https://cdn.jsdelivr.net/gh/prebid/currency-file@1/latest.json', },
-       });
-      ubpbjs.requestBids({
-        bidsBackHandler: initAdserver,
-        timeout: PREBID_TIMEOUT,
-        labels: [GEO_CODE],
-      });
-  });
-  // in case ubpbjs doesn't load
-  setTimeout(function() {
-      initAdserver();
-  }, FAILSAFE_TIMEOUT);
-}
+setTimeout(function() {
+    initAdserver();
+}, FAILSAFE_TIMEOUT);

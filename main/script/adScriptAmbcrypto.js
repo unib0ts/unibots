ed5fe3be02a909b4b8b862ed38fcd5ce7297fea1
bhu15dr <bhu15dr@gmail.com>
2020-12-30 12:41:40 +0530

    amb refrsh disb

--- main/script/adScriptAmbcrypto.js
+++ main/script/adScriptAmbcrypto.js
@@ -1167,7 +1167,7 @@ function checkHBUnits(){
   // }, FAILSAFE_TIMEOUT);
 }
 function callFullHBAds(adCode, ub_slot){
-  fillRefreshMap();
+  // fillRefreshMap();
   ubpbjs.que.push(function(){
     ubpbjs.requestBids({
       timeout: PREBID_TIMEOUT,

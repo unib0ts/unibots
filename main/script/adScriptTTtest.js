792dcd240eeb18ffa4e31a097c961b4d0a01dff5
bhu15dr <bhu15dr@gmail.com>
2021-03-02 16:29:58 +0530

    custom config fxd

--- main/script/adScriptTTtest.js
+++ main/script/adScriptTTtest.js
@@ -30,7 +30,7 @@ var GEO_CODE = '';
 		request.send();
 })();
 
-var customConfigObjectA = {
+const customConfigObjectA = {
  "buckets" : [{
     "precision": 2,  //default is 2 if omitted - means 2.1234 rounded to 2 decimal places = 2.12
     "min" : 0,

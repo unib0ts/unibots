a80c0853e80466670642254ed501bd0d29cd71bf
bhu15dr <bhu15dr@gmail.com>
2021-03-04 19:19:52 +0530

    after dfp

--- main/script/adScriptRN_new.js
+++ main/script/adScriptRN_new.js
@@ -1,19 +1,18 @@
-if(typeof customConfigObjectA === 'undefined'){
+var s4 = document.querySelector('script[src="https://securepubads.g.doubleclick.net/gpt/pubads_impl_2021022501.js?31060314"]');
+s4.onload = function(){
+// if(typeof customConfigObjectA === 'undefined'){
   var mybotstyleSheet='.ub-sticky-ad-container{width:100%;float:left;text-align:center;background:#fff;position:fixed;bottom:0;left:0;box-shadow:0 -3px 3px rgba(0,0,0,.2)!important;z-index:2147483647}.ub-sticky-ad{width:100%;z-index:2147483647;padding-top:4px}.close_ub-sticky-ad{display:none;position:absolute;top:-20px;background:#fff;color:#000;left:0px;pointer-events:all;height:20px;z-index:2147483647;width:30px;font-size:26px;line-height:23px;box-shadow:0 -3px 3px rgba(0,0,0,.2)!important;border-radius:2px 10px 0 0}.close_ub-sticky-addesk{display:none;top: -16px;right: -20px;font-size: 20px;padding: 0px 5px;border-radius: 50%;color: #fff;background: #000;position: absolute;pointer-events:all;cursor:pointer;z-index:2147483647;}.ub-sticky-ad-containerdesk{position:fixed;width:auto!important;bottom:0;left:50%;transform:translateX(-50%);z-index:2147483647}.article-ad-container{margin: 8px 0; clear:both;}';
 	var css=document.createElement('style');
 	css.type='text/css';
 	css.appendChild(document.createTextNode(mybotstyleSheet));
 	document.getElementsByTagName('head')[0].appendChild(css);
 
-
+  var dfpjs = document.querySelector('script[src="https://securepubads.g.doubleclick.net/gpt/pubads_impl_2021022501.js"]');
  function mobileCheck() {
 	var check = false;
 	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
 	return check;
 };
-
-	mybotcentersticky_desk = '<div id="div-gpt-ad-1614757950116-0">';
-	mybotcentersticky_mob ='<div id="div-gpt-ad-1614757921889-0">'
 
   if (!mobileCheck()) {
 		z= document.createElement('div');
@@ -22,7 +21,6 @@ if(typeof customConfigObjectA === 'undefined'){
 		z.innerHTML ='<span class="close_ub-sticky-addesk" id="close_ub-sticky-ad" onclick="mybotubstickyadDesk()">\u0078</span><div class="ub-sticky-adDesk" id="desktopsticky"></div';
 		x = document.querySelector('body');
 		x.appendChild(z);
-		document.getElementById('desktopsticky').innerHTML=mybotcentersticky_desk;
   }
   else {
     z1= document.createElement('div');
@@ -31,7 +29,6 @@ if(typeof customConfigObjectA === 'undefined'){
     z1.innerHTML ='<span class="close_ub-sticky-ad" id="close_ub-sticky-ad" onclick="mybotubstickyad()">\u0078</span><div class="ub-sticky-ad" id="mobilesticky"></div';
     x1 = document.querySelector('body');
     x1.appendChild(z1);
-    document.getElementById('mobilesticky').innerHTML=mybotcentersticky_mob;
   }
 
  function mybotubstickyad() {
@@ -276,7 +273,7 @@ else {
           googletag.cmd.push(function() {
             ubpbjs.que.push(function() {
                 ubpbjs.setTargetingForGPTAsync();
-                // googletag.pubads().refresh([ub_slot]);
+                googletag.pubads().refresh([ub_slot]);
                 // var adsCalled = false;
                 // for(var i=0;i<x.length;i++){
                 //   var bc = x[i].bidderCode;
@@ -301,7 +298,7 @@ else {
       googletag.cmd.push(function() {
           ubpbjs.que.push(function() {
               ubpbjs.setTargetingForGPTAsync();
-              // googletag.pubads().refresh(mappings.slots);
+              googletag.pubads().refresh(mappings.slots);
               // var x = ubpbjs.getAllPrebidWinningBids();
               // var adsCalled = false;
               // for(var i=0;i<x.length;i++){
@@ -391,6 +388,7 @@ else {
 
 
 if (!mobileCheck()) {
+  document.getElementById('desktopsticky').innerHTML='<div id="div-gpt-ad-1614757950116-0">';
 	mappings.slotNumbers.push(2);
   mappings.adCode.push('/21956916242/raftaar.in_hb_dsk_728x90');
   mappings.sizes.push(div_2_sizes);
@@ -404,6 +402,7 @@ if (!mobileCheck()) {
   });
 }
 else {
+  document.getElementById('mobilesticky').innerHTML='<div id="div-gpt-ad-1614757921889-0">';
 	mappings.slotNumbers.push(1);
   mappings.adCode.push('/21956916242/raftaar.in_hb_mb_320x50');
   mappings.sizes.push(div_1_sizes);
@@ -426,5 +425,6 @@ else {
 	    googleDefine(mappings.slotNumbers, mappings.adCode, mappings.sizes, mappings.adId);
 	    googlePush();
 	  }, 500);
-  }
+  // }
+ }
 }

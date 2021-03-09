21aba629ba1d9713670205f8783596ce401444d1
bhu15dr <bhu15dr@gmail.com>
2021-03-09 16:12:53 +0530

    adscrpt

--- main/script/adScriptSample.js
+++ main/script/adScriptSample.js
@@ -1,12 +1,9 @@
-var mybotstyleSheet='.ub-sticky-ad-container{background: url("https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/IrctcWidget/irctc_catering.jpg");background-size: 300px 250px;cursor: pointer;pointer-events: all;position: relative;height: 250px;width: 300px;}';
+var s0 = document.createElement('script');
+s0.type = "text/javascript";
+s0.text = 'google_ad_client = "ca-pub-2730263451308801";google_ad_slot = "bongdaplus_adx_300x250_passback";google_ad_width = 300;google_ad_height = 250;';
+document.getElementsByTagName('head')[0].appendChild(s0);
 
-var css=document.createElement('style');
-css.type='text/css';
-css.appendChild(document.createTextNode(mybotstyleSheet));
-document.getElementsByTagName('head')[0].appendChild(css);
-
-z1= document.createElement('div');
-z1.id = 'ub-sticky-ad-container';
-z1.className = 'ub-sticky-ad-container';
-x1 = document.querySelector('body');
-x1.appendChild(z1);
+var s1 = document.createElement('script');
+s1.type = "text/javascript";
+s1.src = "//pagead2.googlesyndication.com/pagead/show_ads.js";
+document.getElementsByTagName('head')[0].appendChild(s1);

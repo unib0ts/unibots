ed2da3a2ad73dee983830c2333d6a0c91949f9d1
bhu15dr <bhu15dr@gmail.com>
2021-03-09 17:08:39 +0530

    sample ad fxd

--- main/script/adScriptSample.js
+++ main/script/adScriptSample.js
@@ -1,9 +1,9 @@
-var s0 = document.createElement('script');
-s0.type = "text/javascript";
-s0.text = 'google_ad_client = "ca-pub-2730263451308801";google_ad_slot = "bongdaplus_adx_300x250_passback";google_ad_width = 300;google_ad_height = 250;';
-document.getElementsByTagName('head')[0].appendChild(s0);
-
-var s1 = document.createElement('script');
-s1.type = "text/javascript";
-s1.src = "//pagead2.googlesyndication.com/pagead/show_ads.js";
-document.getElementsByTagName('head')[0].appendChild(s1);
+<script type="text/javascript">
+google_ad_client = "ca-pub-2730263451308801";
+/* bongdaplus_adx_300x250_passback */
+google_ad_slot = "bongdaplus_adx_300x250_passback";
+google_ad_width = 300;
+google_ad_height = 250;
+</script>
+<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>

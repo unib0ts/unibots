8a0d617e39d675518eefa582777e0026d9e0f83a
bhu15dr <bhu15dr@gmail.com>
2021-03-10 10:17:16 +0530

    refsh

--- main/script/adScriptDocbao.js
+++ main/script/adScriptDocbao.js
@@ -438,7 +438,7 @@ function checkHBUnits(){
   // }, FAILSAFE_TIMEOUT);
 }
 function callFullHBAds(adCode, ub_slot){
-  fillRefreshMap();
+  // fillRefreshMap();
   ubpbjs.que.push(function(){
     ubpbjs.requestBids({
       timeout: PREBID_TIMEOUT,

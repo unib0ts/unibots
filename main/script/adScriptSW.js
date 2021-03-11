d40e916a8fec15be59b56f8a97045c276cc25093
bhu15dr <bhu15dr@gmail.com>
2021-03-11 16:04:46 +0530

    ad test

--- main/script/adScriptSW.js
+++ main/script/adScriptSW.js
@@ -265,7 +265,7 @@ function refreshBid(ub_slot, adCode) {
         googletag.cmd.push(function() {
           ubpbjs.que.push(function() {
               ubpbjs.setTargetingForGPTAsync();
-              googletag.pubads().refresh([ub_slot1]);
+              // googletag.pubads().refresh([ub_slot1]);
               var adsCalled = false;
               for(var i=0;i<x.length;i++){
                 var bc = x[i].bidderCode;

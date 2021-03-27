d5b13eb0c30f8408d34d6308079a8071c8f561bb
bhu15dr <bhu15dr@gmail.com>
2021-03-27 12:55:41 +0530

    bidr upd

--- main/script/adScriptTorrents_proxy.js
+++ main/script/adScriptTorrents_proxy.js
@@ -52,11 +52,11 @@ const customConfigObjectA = {
       },
       bids: [
        { bidder: 'appnexus', params: { placementId: '21267531' } }, /* one placementId for all sizes  my appnexus bidder */
-       // { bidder: 'sovrn', params: {tagid: '867117'} },
+       { bidder: 'sovrn', params: {tagid: '875620'} },
        // { bidder: 'smartadserver', params: { siteId: '399740', pageId: '1361922', formatId: '93231', domain: 'https://prg8.smartadserver.com' } },
        // // // //{ bidder: 'sonobi', params: { placement_id: 'e061c85c1bf277a0a913', ad_unit: 'ragalahari_NB_728x90' } },
        { bidder: 'onetag', params: { pubId: '60c32c42465aac2' } },
-       // { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '3232632'} },
+       { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '3548535'} },
        { bidder: 'adyoulike', params: { placement: '2c2ca1653a87dd3ebe409bd5efbd611b'}, labelAll: ["US"] },
        { bidder: 'criteo', params: {networkId: '10542'} },
        { bidder: 'criteointl', params: {networkId: '10545'} },

ca9729017d759296e34463395ac2a7bbb74eedcd
bhu15dr <bhu15dr@gmail.com>
2021-04-12 12:20:36 +0530

    tv9

--- main/script/adScriptRozanaspokesman.js
+++ main/script/adScriptRozanaspokesman.js
@@ -155,12 +155,12 @@ if (botsiteurl=='www.rozanaspokesman.in') {
           }
       },
       bids: [
-       // { bidder: 'appnexus', params: { placementId: '20250875' } }, /* one placementId for all sizes  my appnexus bidder */
-       // { bidder: 'sovrn', params: {tagid: '777522'} },
+       { bidder: 'appnexus', params: { placementId: '20250876' } }, /* one placementId for all sizes  my appnexus bidder */
+       { bidder: 'sovrn', params: {tagid: '777522'} },
        // { bidder: 'smartadserver', params: { siteId: '372442', pageId: '1309513', formatId: '93231', domain: 'https://prg8.smartadserver.com' }, labelAny: ["US", "CA"] },
        // // //{ bidder: 'sonobi', params: { placement_id: 'e061c85c1bf277a0a913', ad_unit: 'ragalahari_NB_728x90' } },
        { bidder: 'onetag', params: { pubId: '60c32c42465aac2' } },
-       // // { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '3225513'} },
+       { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '3225513'} },
        { bidder: 'criteo', params: {networkId: '10542'} },
        { bidder: 'criteointl', params: {networkId: '10545'} },
        { bidder: 'oftmedia', params: { placementId: '20846125' } },
@@ -170,7 +170,7 @@ if (botsiteurl=='www.rozanaspokesman.in') {
        // //  // { bidder: 'openx', params: {unit: '541046026', delDomain: 'yieldbird-d.openx.net'} },
        // { bidder: 'rhythmone', params: { placementId: '205945'}}, /* one placementId for all sizes */
        // // // { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
-       //  { bidder: 'nobid', params: { siteId : '22125437326'} },
+        { bidder: 'nobid', params: { siteId : '22125437323'} },
         { bidder: 'adyoulike', params: { placement: '2c2ca1653a87dd3ebe409bd5efbd611b'}, labelAll: ["US"] },
         //{ bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
         // { bidder: 'rubicon', params: {accountId: '11734', siteId: '323604', zoneId: '1680004'} }

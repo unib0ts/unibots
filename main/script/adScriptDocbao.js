f5ded0070df3d279efdb92ae9beb853bcfbbfdb1
bhu15dr <bhu15dr@gmail.com>
2021-04-27 19:04:44 +0530

    link upd

--- main/script/adScriptDocbao.js
+++ main/script/adScriptDocbao.js
@@ -41,32 +41,33 @@ if (!mobileCheck()) {
   ad_scrpt2.innerText= mybotadscrptChild;
   document.querySelector('.footer').appendChild(ad_scrpt2);
 
-  z= document.createElement('div');
-  z.id = 'ub-left-ad';
-  z.className = 'ub-left-ad';
-  x = document.querySelector('body');
-  parentWidth = x.offsetWidth;
-  child = document.querySelector('.header');
-  childWidth = child.offsetWidth;
-  space = (parentWidth - childWidth)/2;
-  if(space > 120){
-  	child.appendChild(z);
-  	document.getElementById('ub-left-ad').innerHTML= '<div id="div-gpt-ad-1615189484195-0">';
-  }
+  if (document.querySelector('.header')) {
+    z= document.createElement('div');
+    z.id = 'ub-left-ad';
+    z.className = 'ub-left-ad';
+    x = document.querySelector('body');
+    parentWidth = x.offsetWidth;
+    child = document.querySelector('.header');
+    childWidth = child.offsetWidth;
+    space = (parentWidth - childWidth)/2;
+    if(space > 120){
+     child.appendChild(z);
+     document.getElementById('ub-left-ad').innerHTML= '<div id="div-gpt-ad-1615189484195-0">';
+    }
 
-  z= document.createElement('div');
-  z.id = 'ub-right-ad';
-  z.className = 'ub-right-ad';
-  x = document.querySelector('body');
-  parentWidth = x.offsetWidth;
-  child = document.querySelector('.header');
-  childWidth = child.offsetWidth;
-  space = (parentWidth - childWidth)/2;
-  if(space > 120){
-  	child.appendChild(z);
-  	document.getElementById('ub-right-ad').innerHTML= '<div id="div-gpt-ad-1615189532474-0">';
+    z= document.createElement('div');
+    z.id = 'ub-right-ad';
+    z.className = 'ub-right-ad';
+    x = document.querySelector('body');
+    parentWidth = x.offsetWidth;
+    child = document.querySelector('.header');
+    childWidth = child.offsetWidth;
+    space = (parentWidth - childWidth)/2;
+    if(space > 120){
+     child.appendChild(z);
+     document.getElementById('ub-right-ad').innerHTML= '<div id="div-gpt-ad-1615189532474-0">';
+    }
   }
-
 }
 else {
   // if(!(window.location.href== "https://m.docbao.vn/") && !(window.location.href.split('https://m.docbao.vn/xa-hoi').length == 2)){

ac73b3bf63af58239bcc556f38312eeb14a1562b
bhu15dr <bhu15dr@gmail.com>
2021-05-29 14:09:14 +0530

    var chgn

--- main/script/adScriptVnexpress.js
+++ main/script/adScriptVnexpress.js
@@ -1,13 +1,4 @@
-var s0 = document.createElement('script');
-s0.src = "https://www.googletagservices.com/tag/js/gpt.js";
-s0.type = "text/javascript";
-document.getElementsByTagName('head')[0].appendChild(s0);
 
-var s1 = document.createElement('script');
-s1.async = "async";
-s1.src = "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/script/adScript.js";
-s1.type = "text/javascript";
-document.getElementsByTagName('head')[0].appendChild(s1);
 
 var PREBID_TIMEOUT = 1200;
 var FAILSAFE_TIMEOUT = 3000;
@@ -192,7 +183,17 @@ var mappings = {
 
 
 function 	ub_ad() {
+  var s0 = document.createElement('script');
+  s0.src = "https://www.googletagservices.com/tag/js/gpt.js";
+  s0.type = "text/javascript";
+  document.getElementsByTagName('head')[0].appendChild(s0);
 
+  var s1 = document.createElement('script');
+  s1.async = "async";
+  s1.src = "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/script/adScript.js";
+  s1.type = "text/javascript";
+  document.getElementsByTagName('head')[0].appendChild(s1);
+  
     var GEO_CODE = '';
     (function (){
       var request = new XMLHttpRequest();

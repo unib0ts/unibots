16372f9daddd030736da6fcb4ab5125210b5c9af
bhu15dr <bhu15dr@gmail.com>
2021-06-22 15:05:10 +0530

    bidr upd

--- main/script/adScriptDocbao.js
+++ main/script/adScriptDocbao.js
@@ -14,7 +14,7 @@ apstag.init({
 });
 apSlots = []
 
-var mybotstyleSheet='.ub-sticky-ad-container{width:100%;float:left;text-align:center;background:#fff;position:fixed;bottom:0;left:0;box-shadow:0 -3px 3px rgba(0,0,0,.2)!important;z-index:100}.ub-sticky-ad{width:100%;z-index:2147483647;pointer-events:all;cursor:pointer;padding-top:4px}.close_ub-sticky-ad{display:none;position:absolute;top:-20px;background:#fff;color:#000;left:0px;pointer-events:all;height:20px;z-index:100;width:30px;font-size:26px;line-height:23px;box-shadow:0 -3px 3px rgba(0,0,0,.2)!important;border-radius:2px 10px 0 0}.close_ub-sticky-addesk{display:block;top: -25px;right:5px;width:18px;position:absolute; pointer-events:all;cursor:pointer;z-index:2147483647;}.ub-sticky-ad-containerdesk{position:fixed;width:auto!important;bottom:0;right:0;z-index:2147483647}.ub-left-ad{width: auto !important;position: fixed;top: 50px;left: 0px;z-index: 1;pointer-events:all;cursor:pointer;}.ub-right-ad{width: auto !important;position: fixed;top: 50px;right: 10px;z-index: 100; } iframe{ vertical-align: unset !important;border: unset !important;margin: unset !important; padding: unset !important; outline-width: unset !important; outline-style: unset !important; }';
+var mybotstyleSheet='.ub-sticky-ad-container{width:100%;float:left;text-align:center;background:#fff;position:fixed;bottom:0;left:0;box-shadow:0 -3px 3px rgba(0,0,0,.2)!important;z-index:100}.ub-sticky-ad{width:100%;z-index:2147483647;pointer-events:all;cursor:pointer;padding-top:4px}.close_ub-sticky-ad{display:none;position:absolute;top:-20px;background:#fff;color:#000;left:0px;pointer-events:all;height:20px;z-index:100;width:30px;font-size:26px;line-height:23px;box-shadow:0 -3px 3px rgba(0,0,0,.2)!important;border-radius:2px 10px 0 0}.close_ub-sticky-addesk{display:block;top: -25px;right:5px;width:18px;position:absolute; pointer-events:all;cursor:pointer;z-index:2147483647;}.ub-sticky-ad-containerdesk{position:fixed;width:auto!important;bottom:0;right:0;z-index:2147483647}.ub-left-ad{width: auto !important;position: fixed;top: 50px;left: 0px;z-index: 1;pointer-events:all;cursor:pointer;}.ub-right-ad{width: auto !important;position: fixed;top: 50px;right: 10px;z-index: 100; } iframe{ vertical-align: unset !important;border: unset !important;margin: unset !important; padding: unset !important; outline-width: unset !important; outline-style: unset !important; } #gpt-ub125 {z-index:2147483647;}';
 
 // var mybotadscrpt = 'var adgf_p1="3766"; var adgf_p2="0"; var adgf_p3="0"; var adgf_p4=""; var adgf_p5=""; var adgFtSc=document.createElement("script"); adgFtSc.id="adg_pixel_footer_script"; document.body.appendChild(adgFtSc);adgFtSc.async=true;var adgScParam="p1="+adgf_p1+"&p2="+adgf_p2+"&p3="+adgf_p3+"&p4="+adgf_p4+"&p5="+adgf_p5;adgFtSc.src="//adgebra.co.in/afpf/afpf.js?"+adgScParam;';
 // var mybotadscrptChild ='<iframe id="adg_footer_pixel_script" src="//adgebra.co.in/afpf/afpf?p1=3766&p2=0&p3=1&p4=&p5=" width="0" height="0" frameBorder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>';
@@ -1164,21 +1164,25 @@ if (mobileCheck()) {
     });
   }
   else {
-    z3= document.createElement('div');
-    z3.innerHTML ='<span class="close_ub-sticky-ad" id="close_ub-sticky-ad" onclick="mybotubstickyadPassbck()">\u0078</span>';
-    x3 = document.getElementById('gpt-ub125');
-    x3.appendChild(z3);
-    document.getElementById('close_ub-sticky-ad').classList.add('close_ub-sticky-ad');
-    googletag.cmd.push(function() {
-          googletag.pubads().addEventListener('slotRenderEnded', function(event) {
-            // if (event.slot === mapping_full_hb.slotNumbers[i]) {
-              var nodes = document.getElementById('gpt-ub125').childNodes[2].childNodes;
-              if(nodes.length && nodes[0].nodeName.toLowerCase() == 'iframe') {
-                document.getElementById('close_ub-sticky-ad').style.display = 'block';
-              }
-            // }
-          });
-    });
+    if (document.getElementById('gpt-ub125')) {
+      z3= document.createElement('div');
+      z3.innerHTML ='<span class="close_ub-sticky-ad" id="close_ub-sticky-ad" onclick="mybotubstickyadPassbck()">\u0078</span>';
+      x3 = document.getElementById('gpt-ub125');
+      x3.appendChild(z3);
+      document.getElementById('close_ub-sticky-ad').classList.add('close_ub-sticky-ad');
+      document.getElementById('close_ub-sticky-ad').style.paddingLeft = "10px";
+      document.getElementById('close_ub-sticky-ad').style.zIndex = '2147483647';
+      googletag.cmd.push(function() {
+            // googletag.pubads().addEventListener('slotRenderEnded', function(event) {
+              // if (event.slot === mapping_full_hb.slotNumbers[i]) {
+                var nodes = document.getElementById('gpt-ub125').childNodes[2].childNodes;
+                if(nodes.length && nodes[0].nodeName.toLowerCase() == 'iframe') {
+                  document.getElementById('close_ub-sticky-ad').style.display = 'block';
+                }
+              // }
+            // });
+      });
+    }
   }
 }
 

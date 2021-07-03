6f907be2229982864d3e27daf7028c57245cc350
Arun Kumar <arunkumarjangra@gmail.com>
2021-07-03 16:39:05 +0530

    docbao video changes

--- ubPlayer/docbao/script.js
+++ ubPlayer/docbao/script.js
@@ -129,7 +129,6 @@ if(isMobile()){
       var vjsOptions = {
         autoplay: false,
         muted: false,
-        bigPlayButtton:false,
         // fluid:true,
         width:320,
         height:240,
@@ -137,7 +136,7 @@ if(isMobile()){
       console.log(vjsOptions);
 
       ubPlayer = videojs('content_video', vjsOptions);
-      ubPlayer.src({ type: "video/mp4", src: "https://cdn.jsdelivr.net/npm/big-buck-bunny-1080p@0.0.6/video.mp4"});
+      ubPlayer.src({ type: "video/mp4", src: "https://s0.2mdn.net/4253510/google_ddm_animation_480P.mp4"});
 
       var imaOptions = {
         id: "content_video",

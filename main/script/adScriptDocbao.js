5e5df494a892c13a5b95203ee0c8b9b72028ede6
bhu15dr <bhu15dr@gmail.com>
2021-07-05 12:37:13 +0530

    div upd

--- main/script/adScriptDocbao.js
+++ main/script/adScriptDocbao.js
@@ -216,7 +216,7 @@ var adUnits = [
             }
         },
         bids: [
-          { bidder: 'appnexus', params: { placementId: '21156521' } }, /* one placementId for all sizes  my appnexus bidder */
+          // { bidder: 'appnexus', params: { placementId: '21156521' } }, /* one placementId for all sizes  my appnexus bidder */
           // { bidder: 'oftmedia', params: { placementId: '20846125' } },
           // // // // { bidder: '33across', params: { siteId : 'bPSPrGBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
           // // // { bidder: 'emx_digital', params: { tagid: '112606' } }, /* sizeless */

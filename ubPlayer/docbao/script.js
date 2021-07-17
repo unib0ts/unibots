5c38fb18a663e680b7826af2a332f7427eb9c3b2
Arun Kumar <arunkumarjangra@gmail.com>
2021-07-17 18:08:02 +0530

    Docbao Player MB size Change

--- ubPlayer/docbao/script.js
+++ ubPlayer/docbao/script.js
@@ -128,8 +128,8 @@ function initPlayer() {
         muted: false,
         loadingSpinner: false,
         bigPlayButton:false,
-        width: isMobile() ? 344 : 400,
-        height: isMobile() ? 258 : 225,
+        width: isMobile() ? 320 : 400,
+        height: isMobile() ? 180 : 225,
         controlBar: {
           volumePanel: {
             inline: false,
@@ -148,9 +148,9 @@ function initPlayer() {
         // adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator=',
         adTagUrl:"https://video.unibots.in/clients/docbao/ads.xml",
         disableCustomPlaybackForIOS10Plus: true,
-        disableAdControls: !0,
+        disableAdControls: !1,
         showControlsForJSAds: !1,
-				showCountdown: !1,
+        showCountdown: !1,
         contribAdsSettings: {
           debug: true,
           timeout: 8000,

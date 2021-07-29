2d734f765724c09787341bc2ff2e74d0c8e8cbbb
Arun Kumar <arunkumarjangra@gmail.com>
2021-07-29 16:18:34 +0530

    madhyamam video chagnes & floating after inread, bongda set attrib removed

--- ubPlayer/laodong/script.js
+++ ubPlayer/laodong/script.js
@@ -118,7 +118,8 @@ function initPlayer() {
   if(document.getElementById('unibots-video') && isMobile()){
       var video = document.getElementById('content_video');
       videojs.browser.IS_IOS ? video.setAttribute('playsinline', '') : '';
-      document.getElementById("ubVideo").click();
+
+      console.log(document.getElementById("ubVideo").click());
 
       var vjsOptions = {
         autoplay: false,
@@ -139,8 +140,8 @@ function initPlayer() {
 
       var imaOptions = {
         id: "content_video",
-        // adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator=',
-        adTagUrl:"https://video.unibots.in/clients/laodong/ads.xml",
+        adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator=',
+        // adTagUrl:"https://video.unibots.in/clients/laodong/ads.xml",
         disableCustomPlaybackForIOS10Plus: true,
         locale: "vn",
         contribAdsSettings: {

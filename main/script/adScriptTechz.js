71e114d7d5c6079dc60292b308767ada0d1dca92
bhu15dr <bhu15dr@gmail.com>
2021-09-08 18:32:04 +0530

    aniview techz

--- main/script/adScriptTechz.js
+++ main/script/adScriptTechz.js
@@ -203,12 +203,19 @@ else{
   apSlots.push(apSlotTemp);
 
 function mybotubScript() {
-  var cachebuster = Math.round(new Date().getTime() / 1000);
-  url ="https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/ubPlayer/techz/script.min.js?cb=" + cachebuster;
-  ub_vs = document.createElement("script");
-  ub_vs.src = url;
-  ub_vs.type = "text/javascript";
-  document.getElementsByTagName("head")[0].appendChild(ub_vs);
+  // var cachebuster = Math.round(new Date().getTime() / 1000);
+  // url ="https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/ubPlayer/techz/script.min.js?cb=" + cachebuster;
+  // ub_vs = document.createElement("script");
+  // ub_vs.src = url;
+  // ub_vs.type = "text/javascript";
+  // document.getElementsByTagName("head")[0].appendChild(ub_vs);
+
+  var s4 = document.createElement('script');
+  s4.async = true;
+  s4.setAttribute("id", "AV61388d7227e73a2a28598054");
+  s4.type = "text/javascript";
+  s4.src = "https://tg1.aniview.com/api/adserver/spt?AV_TAGID=61388d7227e73a2a28598054&AV_PUBLISHERID=5fd9c7d93f2f4c6ef13a1ec7";
+  document.getElementsByTagName('head')[0].appendChild(s4);
 
   var s0 = document.createElement('script');
   s0.src = "https://www.googletagservices.com/tag/js/gpt.js";

c7e26fcadc945d740eb2b018b4bf15b4741e80a6
bhu15dr <bhu15dr@gmail.com>
2021-09-21 17:27:06 +0530

    biddr

--- main/script/adScriptVaartha.js
+++ main/script/adScriptVaartha.js
@@ -89,7 +89,7 @@ var adUnits = [
         	// { bidder: '33across', params: { siteId : 'dTDY7cNiqr64oSaKjGFx_2', productId: 'siab' }, labelAll: ["US"] }, /*All sizes*/
         	// // { bidder: 'emx_digital', params: { tagid: '107931' } }, /* sizeless */
           // { bidder: 'sovrn', params: {tagid: '882931'} },
-          // { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '3910484'} },
+          { bidder: 'pubmatic', params: { publisherId : '866236', adSlot: '4051666'} },
           // { bidder: 'openx', params: {unit: '544096396', delDomain: 'unibots-d.openx.net'}, labelAny: ["US", "CA"] },
         	// // // { bidder: 'rhythmone', params: { placementId: '205945'}}, /* one placementId for all sizes */
         	// // // // { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },

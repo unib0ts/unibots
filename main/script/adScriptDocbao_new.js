b75700dbbfedd07cd23036d3ae733f75cc508661
bhu15dr <bhu15dr@gmail.com>
2021-09-23 11:10:03 +0530

    anchr off

--- main/script/adScriptDocbao_new.js
+++ main/script/adScriptDocbao_new.js
@@ -167,16 +167,16 @@ function ubadScript() {
       x1.appendChild(z1);
     // }
   } else {
-      if(window.location.href.split('https://m.docbao.vn/giai-tri').length == 2 || window.location.href.split('https://m.docbao.vn/doi-song').length == 2 || window.location.href.split('https://m.docbao.vn/kinh-te').length == 2){}else {
-        if(!(window.location.href== "https://m.docbao.vn/")){
-            z1 = document.createElement("div");
-            z1.id = "ub-sticky-ad-container";
-            z1.className = "ub-sticky-ad-container";
-            z1.innerHTML =
-                '<span class="close_ub-sticky-ad" id="close_ub-sticky-ad" onclick="mybotubstickyad()">\u0078</span><div class="ub-sticky-ad" id="div-gpt-ad-1625034565032-0"></div>';
-            x1 = document.querySelector("body");
-            x1.appendChild(z1);
-        }else {
+      if(window.location.href.split('https://m.docbao.vn/giai-tri').length == 2 || window.location.href.split('https://m.docbao.vn/doi-song').length == 2 || window.location.href.split('https://m.docbao.vn/kinh-te').length == 2 || window.location.href== "https://m.docbao.vn/"){}else {
+        // if(!(window.location.href== "https://m.docbao.vn/")){
+        //     z1 = document.createElement("div");
+        //     z1.id = "ub-sticky-ad-container";
+        //     z1.className = "ub-sticky-ad-container";
+        //     z1.innerHTML =
+        //         '<span class="close_ub-sticky-ad" id="close_ub-sticky-ad" onclick="mybotubstickyad()">\u0078</span><div class="ub-sticky-ad" id="div-gpt-ad-1625034565032-0"></div>';
+        //     x1 = document.querySelector("body");
+        //     x1.appendChild(z1);
+        // }else {
             z1 = document.createElement("div");
             z1.id = "ub-sticky-ad-container";
             z1.className = "ub-sticky-ad-container";
@@ -184,7 +184,7 @@ function ubadScript() {
                 '<span class="close_ub-sticky-ad" id="close_ub-sticky-ad" onclick="mybotubstickyad()">\u0078</span><div class="ub-sticky-ad" id="div-gpt-ad-1616569480504-0"></div>';
             x1 = document.querySelector("body");
             x1.appendChild(z1);
-        }
+        // }
       }
 
     if(!(window.location.href== "https://m.docbao.vn/")){
@@ -1443,19 +1443,19 @@ function ubadScript() {
 }
 
  if (mobileCheck()) {
-   if(window.location.href.split('https://m.docbao.vn/giai-tri').length == 2 || window.location.href.split('https://m.docbao.vn/doi-song').length == 2 || window.location.href.split('https://m.docbao.vn/kinh-te').length == 2){}else {
-    if(!(window.location.href== "https://m.docbao.vn/")){
-        googletag.cmd.push(function () {
-            googletag.pubads().addEventListener("slotRenderEnded", function (event) {
-              if (event.slot.getAdUnitPath() ==="/22140546871/docbao.vn_mb_anchor_320x50"){
-                    var nodes_anchr = document.getElementById("div-gpt-ad-1625034565032-0").childNodes[0].childNodes;
-                    if (nodes_anchr.length &&nodes_anchr[0].nodeName.toLowerCase() == "iframe") {
-                        document.getElementById("close_ub-sticky-ad").style.display = "block";
-                    }
-              }
-            });
-        });
-    }else {
+   if(window.location.href.split('https://m.docbao.vn/giai-tri').length == 2 || window.location.href.split('https://m.docbao.vn/doi-song').length == 2 || window.location.href.split('https://m.docbao.vn/kinh-te').length == 2 || window.location.href== "https://m.docbao.vn/"){}else {
+    // if(!(window.location.href== "https://m.docbao.vn/")){
+    //     googletag.cmd.push(function () {
+    //         googletag.pubads().addEventListener("slotRenderEnded", function (event) {
+    //           if (event.slot.getAdUnitPath() ==="/22140546871/docbao.vn_mb_anchor_320x50"){
+    //                 var nodes_anchr = document.getElementById("div-gpt-ad-1625034565032-0").childNodes[0].childNodes;
+    //                 if (nodes_anchr.length &&nodes_anchr[0].nodeName.toLowerCase() == "iframe") {
+    //                     document.getElementById("close_ub-sticky-ad").style.display = "block";
+    //                 }
+    //           }
+    //         });
+    //     });
+    // }else {
       googletag.cmd.push(function () {
           googletag.pubads().addEventListener("slotRenderEnded", function (event) {
             if (event.slot.getAdUnitPath() ==="/22140546871/docbao.vn_mb_anchor_320x100"){
@@ -1466,7 +1466,7 @@ function ubadScript() {
             }
           });
       });
-    }
+    // }
   }
 }
 

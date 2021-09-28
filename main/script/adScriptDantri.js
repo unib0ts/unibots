a40fa4213ca8b7035584e62778abfda4c24dd679
bhu15dr <bhu15dr@gmail.com>
2021-09-28 18:23:41 +0530

    dantir

--- main/script/adScriptDantri.js
+++ main/script/adScriptDantri.js
@@ -101,33 +101,6 @@ var FAILSAFE_TIMEOUT = 3000;
 var REFRESH_TIMEOUT = 30000;
 // var boturlbid = window.location.hostname;
 
-var GEO_CODE = '';
-(function (){
-  var request = new XMLHttpRequest();
-		url = 'https://pro.ip-api.com/json/?fields=status,message,countryCode&key=LWKtz4EzQwMJRyQ';
-		request.open('GET', url, true);
-		request.onload = function() {
-			if (request.status >= 200 && request.status < 400) {
-				var data = request.responseText;
-				data = JSON.parse(data);
-				if(data.status == "success") {
-          GEO_CODE = data.countryCode;
-				}
-				else {
-					console.error("Geo Request Failed");
-				}
-			}
-			else {
-				console.error('Request failed from server');
-			}
-      mainHbRun();
-		};
-		request.onerror = function() {
-			console.error('Request failed to Reach GEO Server');
-      mainHbRun();
-		};
-		request.send();
-})();
 
 const customConfigObjectA = {
  "buckets" : [{
@@ -211,6 +184,97 @@ function mybotubScript() {
   s1.type = "text/javascript";
   document.getElementsByTagName('head')[0].appendChild(s1);
 
+  var GEO_CODE = '';
+  (function (){
+    var request = new XMLHttpRequest();
+  		url = 'https://pro.ip-api.com/json/?fields=status,message,countryCode&key=LWKtz4EzQwMJRyQ';
+  		request.open('GET', url, true);
+  		request.onload = function() {
+  			if (request.status >= 200 && request.status < 400) {
+  				var data = request.responseText;
+  				data = JSON.parse(data);
+  				if(data.status == "success") {
+            GEO_CODE = data.countryCode;
+  				}
+  				else {
+  					console.error("Geo Request Failed");
+  				}
+  			}
+  			else {
+  				console.error('Request failed from server');
+  			}
+        mainHbRun();
+  		};
+  		request.onerror = function() {
+  			console.error('Request failed to Reach GEO Server');
+        mainHbRun();
+  		};
+  		request.send();
+  })();
+
+  function mainHbRun(){
+    ubpbjs.que.push(function() {
+      ubpbjs.addAdUnits(adUnits);
+      ubpbjs.aliasBidder('criteo','criteointl');
+      ubpbjs.bidderSettings = {
+        'appnexus': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.86; } },
+        'pubmatic': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.74; } },
+        'rubicon': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
+        'openx': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
+        'criteo': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+        'criteointl': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+        'nobid': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+        'oftmedia': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.80; } },
+        'sovrn': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.81; } },
+        'onetag': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
+        //'adsolut': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+
+        '33across': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+        'emx_digital': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+        'rhythmone': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
+        'eplanning': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } }
+      };
+      ubpbjs.setConfig({
+        priceGranularity: customConfigObjectA,
+        userSync: {
+            iframeEnabled: true,
+            syncsPerBidder: 999, // and no more than 3 syncs at a time
+            // syncDelay: PREBID_TIMEOUT*4, // 5 seconds after the auction
+            filterSettings: { iframe: { bidders: [''], filter: 'exclude' }, image:  { bidders: '*', filter: 'include' } },
+            // enableOverride: true // publisher will call `ubpbjs.triggerUserSyncs()'
+            userIds: [{
+                name: "id5Id",
+                params: {
+                    partner: 438,            // change to the Partner Number you received from ID5
+                },
+                storage: {
+                    type: "cookie",
+                    name: "id5id.1st",       // create a cookie with this name
+                    expires: 90,             // cookie lasts for 90 days
+                    refreshInSeconds: 8*3600 // refresh ID every 8 hours to ensure it is fresh
+                }
+            }],
+            auctionDelay: 500},
+        debug: false,
+        useBidCache: true,
+        enableSendAllBids: false, // Default will be `true` as of 1.0
+        bidderSequence: 'random', // Default is random
+        publisherDomain: 'https://dantri.com.vn/',
+        bidderTimeout: PREBID_TIMEOUT+500,
+        //pubcid: {expInterval: },
+        //currency: { 'adServerCurrency': "GBP", 'granularityMultiplier': 1, 'conversionRateFile': 'https://cdn.jsdelivr.net/gh/prebid/currency-file@1/latest.json', },
+       });
+      ubpbjs.requestBids({
+            bidsBackHandler: initAdserver,
+            timeout: PREBID_TIMEOUT,
+            labels: [GEO_CODE],
+        });
+    });
+    setTimeout(function() {
+        initAdserver();
+    }, FAILSAFE_TIMEOUT);
+  }
+
   if (mobileCheck()) {
     mappings.slotNumbers.push(1);
     mappings.adCode.push('/21928950349/dantri.com.vn_mb_anchor_320x50');
@@ -238,69 +302,6 @@ function mybotubScript() {
     });
   // }
   }
-}
-
-function mainHbRun(){
-  ubpbjs.que.push(function() {
-    ubpbjs.addAdUnits(adUnits);
-    ubpbjs.aliasBidder('criteo','criteointl');
-    ubpbjs.bidderSettings = {
-      'appnexus': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.86; } },
-      'pubmatic': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.74; } },
-      'rubicon': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
-      'openx': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.75; } },
-      'criteo': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-      'criteointl': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-      'nobid': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-      'oftmedia': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.80; } },
-      'sovrn': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.81; } },
-      'onetag': { bidCpmAdjustment: function(bidCpm){ return bidCpm*0.85; } },
-      //'adsolut': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-
-      '33across': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-      'emx_digital': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-      'rhythmone': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } },
-      'eplanning': { bidCpmAdjustment: function(bidCpm){ return bidCpm*1.00; } }
-    };
-    ubpbjs.setConfig({
-      priceGranularity: customConfigObjectA,
-      userSync: {
-          iframeEnabled: true,
-          syncsPerBidder: 999, // and no more than 3 syncs at a time
-          // syncDelay: PREBID_TIMEOUT*4, // 5 seconds after the auction
-          filterSettings: { iframe: { bidders: [''], filter: 'exclude' }, image:  { bidders: '*', filter: 'include' } },
-          // enableOverride: true // publisher will call `ubpbjs.triggerUserSyncs()'
-          userIds: [{
-              name: "id5Id",
-              params: {
-                  partner: 438,            // change to the Partner Number you received from ID5
-              },
-              storage: {
-                  type: "cookie",
-                  name: "id5id.1st",       // create a cookie with this name
-                  expires: 90,             // cookie lasts for 90 days
-                  refreshInSeconds: 8*3600 // refresh ID every 8 hours to ensure it is fresh
-              }
-          }],
-          auctionDelay: 500},
-      debug: false,
-      useBidCache: true,
-      enableSendAllBids: false, // Default will be `true` as of 1.0
-      bidderSequence: 'random', // Default is random
-      publisherDomain: 'https://dantri.com.vn/',
-      bidderTimeout: PREBID_TIMEOUT+500,
-      //pubcid: {expInterval: },
-      //currency: { 'adServerCurrency': "GBP", 'granularityMultiplier': 1, 'conversionRateFile': 'https://cdn.jsdelivr.net/gh/prebid/currency-file@1/latest.json', },
-     });
-    ubpbjs.requestBids({
-          bidsBackHandler: initAdserver,
-          timeout: PREBID_TIMEOUT,
-          labels: [GEO_CODE],
-      });
-  });
-  setTimeout(function() {
-      initAdserver();
-  }, FAILSAFE_TIMEOUT);
 }
 
 function ub_checkAdRendered(adId, ub_slot, adCode){

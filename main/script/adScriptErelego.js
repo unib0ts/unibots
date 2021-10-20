7884f69a9f45f6c2e93d59219da81e8be3b3166f
bhu15dr <bhu15dr@gmail.com>
2021-10-20 16:18:32 +0530

    newwdiget

--- main/script/adScriptErelego.js
+++ main/script/adScriptErelego.js
@@ -1,53 +1,42 @@
 mybotBlockedClientsName = ['docbao', 'bongdaplus', 'dantri'];
 
 mybotDomainUrl = (window.location.host).split(".");
-var mybotBlockedClientName = mybotDomainUrl.filter(mybotDomainUrl => mybotBlockedClientsName.includes(mybotDomainUrl));
+mybotBlockedClientName = mybotDomainUrl.filter(mybotDomainUrl => mybotBlockedClientsName.includes(mybotDomainUrl));
 mybotBlockedClientName = mybotBlockedClientName.toString();
 
 mybotBlockedPagesFlag = 1;
-mybotBlockedUrl = 'https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/blocks/blocks'+capitalizeFLetter(mybotBlockedClientName) + '.json';
-console.log(mybotBlockedUrl);
+mybotBlockedUrl = 'https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/blocks/blocks' + capitalizeFLetter(mybotBlockedClientName) + '.json';
 
-if(typeof mybotBlockedPagesFlag !== 'undefined' && mybotBlockedPagesFlag ==1){
-  urlToCheck = window.location.host+window.location.pathname+window.location.search;
+var returnValue = checkBlocked(mybotBlockedUrl, mybotBlockedClientName);
+returnValue.then(() => {
+  (result) =>{
+      console.log('Page is allowed for UBP');
+  }
+})
+console.log(returnValue);
 
-  var request = new XMLHttpRequest();
-  url = mybotBlockedUrl;
-
-  request.open('GET', url, true);
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      var data = request.responseText;
-      data = JSON.parse(data);
-      data = data[mybotBlockedClientName];
-      if(data) {
-        data = data.urls;
-        if(data.includes(urlToCheck)){
-          console.log('Player is not allowed');
-          return false;
-        }
-        else{
-          ubadScript();
-        }
-      }
-    }
-    else {
-      console.log('Block Check Request failed');
-      ubadScript();
-    }
-  };
-  request.onerror = function() {
-    console.log('Request failed');
-    ubadScript();
-  };
-  request.send();
-}
-else{
-  ubadScript();
-}
-
-function ubadScript() {
-  console.log('Player allowed');
+function checkBlocked(url, clientName) {
+  return new Promise((resolve, reject) => {
+      let urlToCheck = window.location.host + window.location.pathname + window.location.search;
+      // let urlToCheck = "bongtrend.com/sreelekha-mitra-showing-off-her-bra-strap";
+      fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              data = data[clientName];
+              if (data) {
+                  data = data.urls;
+                  if (data.includes(urlToCheck)) {
+                      console.log('Page is blocked');
+                      reject('Page is blocked');
+                  }
+                  else {
+                      resolve('Page is allowed');
+                  }
+              }
+          }).catch(() => {
+              resolve('Block Check Request failed')
+          });
+  });
 }
 
 function capitalizeFLetter(input) {

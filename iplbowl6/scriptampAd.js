222a9c97869b723696979381334e0789f399315b
bhu15dr <bhu15dr@gmail.com>
2021-10-24 12:08:35 +0530

    ads upd

---
+++ iplbowl6/scriptampAd.js
@@ -0,0 +1,23 @@
+if(typeof colombia == 'undefined'){
+  var colombia = colombia || {};
+  colombia.fns = colombia.fns || [];
+    (function() {
+        var cads = document.createElement("script");
+        cads.async = true;
+        cads.type = "text/javascript";
+        cads.src = "https://static.clmbtech.com/ase/91170/3748/c1.js";
+        document.getElementsByTagName('head')[0].appendChild(cads);
+    })();
+  }
+
+  ub_passback = document.createElement("div");
+  ub_passback.setAttribute("style", "float:left;min-height:2px;width:100%;");
+  ub_passback.setAttribute("data-position", 1);
+  ub_passback.setAttribute("data-section", "StroyPage");
+  ub_passback.setAttribute("class", "colombia");
+
+  ub_passback.setAttribute("data-ua", "M");
+  ub_passback.setAttribute("data-slot", "463960");
+  ub_passback.setAttribute("id", "div-clmb-ctn-463960-1");
+  ub_sticky = document.querySelector('body');
+  ub_sticky.appendChild(ub_passback);

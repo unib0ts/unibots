292cc309c7c5b2bb8207f8ce1b39774dcae0dc37
bhu15dr <bhu15dr@gmail.com>
2021-11-27 17:06:11 +0530

    player script upd

--- main/script/cubeScript123telugu_new.js
+++ main/script/cubeScript123telugu_new.js
@@ -82,7 +82,7 @@ mybotgabywa = '<script async src="https://www.googletagmanager.com/gtag/js?id='+
          setAttributes(ub_passback, {
            "style":"float:left;min-height:2px;width:auto;position:absolute;bottom:0;",
            "data-position": "1",
-           "data-section": "portal_islam",
+           "data-section": "StroyPage",
            "class": "colombia"
          })
 

a21cbce5b9b1c95f43272e62a1822dc5eeba23b1
bhu15dr <bhu15dr@gmail.com>
2021-12-10 17:12:44 +0530

    script popup enb

--- main/script/adScriptSandesh.js
+++ main/script/adScriptSandesh.js
@@ -19,17 +19,38 @@ var PREBID_TIMEOUT = 2000;
 var FAILSAFE_TIMEOUT = 3000;
 var REFRESH_TIMEOUT = 60000;
 
-// if (window.location.href == "https://sandesh.com/gujarat-st-workers-go-on-strike-for-tomorrow-night/") {
-//     url ="https://cdn.unibots.in/player.js";
-//     ub_vs = document.createElement("script");
-//     ub_vs.src = url;
-//     ub_vs.async = "async";
-//     ub_vs.type = "text/javascript";
-//     document.getElementsByTagName("head")[0].appendChild(ub_vs);
-// }
+function mobileCheck() {
+    var check = false;
+    (function (a) {
+        if (
+            /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+                a
+            ) ||
+            /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+                a.substr(0, 4)
+            )
+        )
+            check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+}
+
+if (mobileCheck() && GEO_CODE =="US") {
+  z2 = document.createElement("div");
+  z2.id = "ub-popup-ad-container";
+  z2.className = "ub-popup-ad-container";
+  z2.innerHTML =
+      '<span class="mybotpopupCloseButton" id="mybotpopupCloseButton" onclick="mybotubPopadcls()" style="display:none">\u0078</span><div class="ub-popup-ad" id="div-gpt-ad-1629457284866-0"><script>googletag.cmd.push(function() {googletag.display("div-gpt-ad-1629457284866-0);});</script></div>';
+  x2 = document.querySelector("body");
+  x2.appendChild(z2);
+}
+
+function mybotubPopadcls() {
+    document.getElementById("ub-popup-ad-container").style.display = "none";
+}
 
 var GEO_CODE = '';
-(function (){
+function ub_geocode(){
   var request = new XMLHttpRequest();
 		url = 'https://pro.ip-api.com/json/?fields=status,message,countryCode&key=LWKtz4EzQwMJRyQ';
 		request.open('GET', url, true);
@@ -54,7 +75,8 @@ var GEO_CODE = '';
       mainHbRun();
 		};
 		request.send();
-})();
+};
+ub_geocode();
 
 const customConfigObjectA = {
  "buckets" : [{
@@ -66,6 +88,7 @@ const customConfigObjectA = {
 };
 
 var ub_div_1_sizes = [320, 50];
+var ub_div_2_sizes = [[320, 480], [480, 320]];
 var ub_adUnits = [];
 
   ub_adUnits1 =
@@ -137,6 +160,35 @@ var ub_adUnits = [];
   };
   ub_adUnits.push(ub_adUnits1);
 
+  if (mobileCheck() && GEO_CODE =="US") {
+      ub_adUnits1 = {
+          code: "/21928950349,22624158342/sandesh_interstitial",
+          mediaTypes: {
+              banner: {
+                  sizes: div_2_sizes,
+              },
+          },
+          bids: [
+                // { bidder: 'appnexus', params: { placementId: '21146574' } }, /* one placementId for all sizes  my appnexus bidder */
+              	// { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '3910487'} },
+                // { bidder: 'oftmedia', params: { placementId: '20846125' } },
+                // // // // { bidder: '33across', params: { siteId : 'bPSPrGBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
+                // // // { bidder: 'emx_digital', params: { tagid: '112606' } }, /* sizeless */
+                // // // // { bidder: 'rhythmone', params: { placementId: '205945'} }, /* one placementId for all sizes */
+                // { bidder: 'nobid', params: { siteId: '22049997436' } },
+                // // // { bidder: 'openx', params: {unit: '545727976', delDomain: 'unibots-d.openx.net'}, labelAny: ["US", "CA"] },
+                // // // // { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
+                // // // // //{ bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
+                // // // { bidder: 'criteo', params: {networkId: '10542'} },
+                // // // { bidder: 'criteointl', params: {networkId: '10545'} },
+                // { bidder: 'onetag', params: { pubId: '60c32c42465aac2' } },
+                // { bidder: 'adyoulike', params: { placement: '2c2ca1653a87dd3ebe409bd5efbd611b'}, labelAll: ["US"] },
+          ],
+      };
+      ub_adUnits.push(ub_adUnits1);
+  }
+
+
 // ======== DO NOT EDIT BELOW THIS LINE =========== //
 var googletag = googletag || {};
 googletag.cmd = googletag.cmd || [];
@@ -153,7 +205,7 @@ var ubmappings = {
   slotNumbers: [],
   sizes: [],
   adId: [],
-  renderedFlag: [false]
+  renderedFlag: [false, false]
 };
 
 // apSlotTemp = {
@@ -373,6 +425,42 @@ googletag.cmd.push(function() {
     }
   });
 });
+
+if (mobileCheck() && GEO_CODE =="US") {
+  ubmappings.slotNumbers.push(2);
+  ubmappings.adCode.push('/21928950349,22624158342/sandesh_interstitial');
+  ubmappings.sizes.push(ub_div_2_sizes);
+  ubmappings.adId.push('div-gpt-ad-1639132029151-0');
+  googletag.cmd.push(function() {
+    // callAPStagBids(); //Ap part
+    // callAPSAds(ubmappings.adCode, ubmappings.slots);
+    googletag.pubads().addEventListener('slotRenderEnded', function(event) {
+      if (event.slot === ub_slot2) {
+        ub_checkAd1Rendered('div-gpt-ad-1639132029151-0', ub_slot2, ['/21928950349,22624158342/sandesh_interstitial']);
+      }
+    });
+  });
+}
+
+function ub_checkAd1Rendered() {
+    adId1 = "div-gpt-ad-1639132029151-0";
+    var nodes = document.getElementById(adId1).childNodes[0].childNodes;
+    if (
+        !(nodes.length && nodes[0].nodeName.toLowerCase() == "iframe") ||
+        document.getElementById(adId1).style.display == "none"
+    ) {
+        document.querySelector("#ub-popup-ad-container").style.display = "none";
+    } else if (nodes.length && nodes[0].nodeName.toLowerCase() == "iframe") {
+        document.getElementById("mybotpopupCloseButton").style.display =
+            "block";
+        document.querySelector("#ub-popup-ad-container").style.display =
+            "block";
+        mybotpopupad = document.querySelector("#ub-popup-ad-container");
+        mybotpopupad.style.height = "100%";
+        mybotpopupad.style.backgroundColor = "rgb(0, 0, 0)";
+        mybotpopupad.style.zIndex = "2147483647";
+    }
+}
 
 if(typeof googletag.defineSlot === "function"){
   googleDefine(ubmappings.slotNumbers, ubmappings.adCode, ubmappings.sizes, ubmappings.adId);

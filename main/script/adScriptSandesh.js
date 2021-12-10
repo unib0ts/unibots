cb3697621cc30ac866aa2daeac525694bebc5eb6
bhu15dr <bhu15dr@gmail.com>
2021-12-10 18:36:54 +0530

    popup fxd

--- main/script/adScriptSandesh.js
+++ main/script/adScriptSandesh.js
@@ -36,7 +36,7 @@ function mobileCheck() {
 }
 
 var GEO_CODE = '';
-function ub_geocode(){
+(function(){
   var request = new XMLHttpRequest();
 		url = 'https://pro.ip-api.com/json/?fields=status,message,countryCode&key=LWKtz4EzQwMJRyQ';
 		request.open('GET', url, true);
@@ -45,7 +45,10 @@ function ub_geocode(){
 				var data = request.responseText;
 				data = JSON.parse(data);
 				if(data.status == "success") {
-          GEO_CODE = data.countryCode;
+           GEO_CODE = data.countryCode;
+           if (GEO_CODE=="US") {
+              ubpopup();
+           }
 				}
 				else {
 					console.error("Geo Request Failed");
@@ -61,8 +64,7 @@ function ub_geocode(){
       mainHbRun();
 		};
 		request.send();
-};
-ub_geocode();
+})();
 
 const customConfigObjectA = {
  "buckets" : [{
@@ -77,20 +79,10 @@ var ub_div_1_sizes = [320, 50];
 var ub_div_2_sizes = [[320, 480], [480, 320]];
 var ub_adUnits = [];
 
-if (mobileCheck() && GEO_CODE =="US") {
-  z2 = document.createElement("div");
-  z2.id = "ub-popup-ad-container";
-  z2.className = "ub-popup-ad-container";
-  z2.innerHTML =
-      '<span class="mybotpopupCloseButton" id="mybotpopupCloseButton" onclick="mybotubPopadcls()" style="display:none">\u0078</span><div class="ub-popup-ad" id="div-gpt-ad-1629457284866-0"><script>googletag.cmd.push(function() {googletag.display("div-gpt-ad-1629457284866-0);});</script></div>';
-  x2 = document.querySelector("body");
-  x2.appendChild(z2);
-}
 
 function mybotubPopadcls() {
     document.getElementById("ub-popup-ad-container").style.display = "none";
 }
-
 
   ub_adUnits1 =
   {
@@ -160,36 +152,6 @@ function mybotubPopadcls() {
       ]
   };
   ub_adUnits.push(ub_adUnits1);
-
-  console.log(GEO_CODE);
-
-  if (mobileCheck() && GEO_CODE =="US") {
-      ub_adUnits1 = {
-          code: "/21928950349,22624158342/sandesh_interstitial",
-          mediaTypes: {
-              banner: {
-                  sizes: div_2_sizes,
-              },
-          },
-          bids: [
-                // { bidder: 'appnexus', params: { placementId: '21146574' } }, /* one placementId for all sizes  my appnexus bidder */
-              	// { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '3910487'} },
-                // { bidder: 'oftmedia', params: { placementId: '20846125' } },
-                // // // // { bidder: '33across', params: { siteId : 'bPSPrGBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
-                // // // { bidder: 'emx_digital', params: { tagid: '112606' } }, /* sizeless */
-                // // // // { bidder: 'rhythmone', params: { placementId: '205945'} }, /* one placementId for all sizes */
-                // { bidder: 'nobid', params: { siteId: '22049997436' } },
-                // // // { bidder: 'openx', params: {unit: '545727976', delDomain: 'unibots-d.openx.net'}, labelAny: ["US", "CA"] },
-                // // // // { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
-                // // // // //{ bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
-                // // // { bidder: 'criteo', params: {networkId: '10542'} },
-                // // // { bidder: 'criteointl', params: {networkId: '10545'} },
-                // { bidder: 'onetag', params: { pubId: '60c32c42465aac2' } },
-                // { bidder: 'adyoulike', params: { placement: '2c2ca1653a87dd3ebe409bd5efbd611b'}, labelAll: ["US"] },
-          ],
-      };
-      ub_adUnits.push(ub_adUnits1);
-  }
 
 
 // ======== DO NOT EDIT BELOW THIS LINE =========== //
@@ -429,7 +391,43 @@ googletag.cmd.push(function() {
   });
 });
 
-if (mobileCheck() && GEO_CODE =="US") {
+function ubpopup() {
+  if (mobileCheck()) {
+    z2 = document.createElement("div");
+    z2.id = "ub-popup-ad-container";
+    z2.className = "ub-popup-ad-container";
+    z2.innerHTML =
+        '<span class="mybotpopupCloseButton" id="mybotpopupCloseButton" onclick="mybotubPopadcls()" style="display:none">\u0078</span><div class="ub-popup-ad" id="div-gpt-ad-1639132029151-0"></div>';
+    x2 = document.querySelector("body");
+    x2.appendChild(z2);
+
+    ub_adUnits1 = {
+        code: "/21928950349,22624158342/sandesh_interstitial",
+        mediaTypes: {
+            banner: {
+                sizes: ub_div_2_sizes,
+            },
+        },
+        bids: [
+              // { bidder: 'appnexus', params: { placementId: '21146574' } }, /* one placementId for all sizes  my appnexus bidder */
+              // { bidder: 'pubmatic', params: { publisherId : '159448', adSlot: '3910487'} },
+              // { bidder: 'oftmedia', params: { placementId: '20846125' } },
+              // // // // { bidder: '33across', params: { siteId : 'bPSPrGBuar6PWLaKlId8sQ', productId: 'siab' } }, /*All sizes*/
+              // // // { bidder: 'emx_digital', params: { tagid: '112606' } }, /* sizeless */
+              // // // // { bidder: 'rhythmone', params: { placementId: '205945'} }, /* one placementId for all sizes */
+              // { bidder: 'nobid', params: { siteId: '22049997436' } },
+              // // // { bidder: 'openx', params: {unit: '545727976', delDomain: 'unibots-d.openx.net'}, labelAny: ["US", "CA"] },
+              // // // // { bidder: 'eplanning', params: { ci: '2cfed', ml: '1' } },
+              // // // // //{ bidder: 'adsolut', params: {zoneId: '107071', host: 'cpm.adsolut.in'} },
+              // // // { bidder: 'criteo', params: {networkId: '10542'} },
+              // // // { bidder: 'criteointl', params: {networkId: '10545'} },
+              // { bidder: 'onetag', params: { pubId: '60c32c42465aac2' } },
+              // { bidder: 'adyoulike', params: { placement: '2c2ca1653a87dd3ebe409bd5efbd611b'}, labelAll: ["US"] },
+        ],
+    };
+    ub_adUnits.push(ub_adUnits1);
+  }
+
   ubmappings.slotNumbers.push(2);
   ubmappings.adCode.push('/21928950349,22624158342/sandesh_interstitial');
   ubmappings.sizes.push(ub_div_2_sizes);

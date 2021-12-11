061849cd0d1f2a0de150d010ed820419eafe2fd4
bhu15dr <bhu15dr@gmail.com>
2021-12-11 14:55:25 +0530

    prebit timeout increase

--- main/script/adScriptGogoanime.js
+++ main/script/adScriptGogoanime.js
@@ -67,7 +67,7 @@ s1.src = "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/script/adScrip
 s1.type = "text/javascript";
 document.getElementsByTagName("head")[0].appendChild(s1);
 
-var PREBID_TIMEOUT = 3500;
+var PREBID_TIMEOUT = 3000;
 var FAILSAFE_TIMEOUT = 3000;
 var REFRESH_TIMEOUT = 60000;
 

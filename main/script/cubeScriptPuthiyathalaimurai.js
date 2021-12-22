a9951f9dbaa7cc0fa7a4d8b1e75b115d76ce55b6
bhu15dr <bhu15dr@gmail.com>
2021-12-22 11:45:13 +0530

    refrsh disbled

--- main/script/cubeScriptPuthiyathalaimurai.js
+++ main/script/cubeScriptPuthiyathalaimurai.js
@@ -177,21 +177,21 @@ function ubadScript() {
     }
   }
 
-  setInterval(function(){
-    if (ub_innerDoc.querySelector('#cube3dSwiper') !=null) {
-      if (!mobileCheck()) {
-        try{
-          colombia.refresh('div-clmb-ctn-467893-1');
-          cubespinnervisibility();
-        }catch(e){}
-      }else {
-        try{
-          colombia.refresh('div-clmb-ctn-467894-1');
-          cubespinnervisibility();
-        }catch(e){}
-      }
-     }
-  }, 30000);
+  // setInterval(function(){
+  //   if (ub_innerDoc.querySelector('#cube3dSwiper') !=null) {
+  //     if (!mobileCheck()) {
+  //       try{
+  //         colombia.refresh('div-clmb-ctn-467893-1');
+  //         cubespinnervisibility();
+  //       }catch(e){}
+  //     }else {
+  //       try{
+  //         colombia.refresh('div-clmb-ctn-467894-1');
+  //         cubespinnervisibility();
+  //       }catch(e){}
+  //     }
+  //    }
+  // }, 30000);
 
 
   function ub_refresh(clmbdiv_id, clmbiframe_id, ub_clmb ) {

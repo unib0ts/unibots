8e776164a4ea4902c1c287c75e5cadc7381e563d
bhu15dr <bhu15dr@gmail.com>
2021-12-24 16:30:19 +0530

    script upd

--- main/script/adScriptGogoanime.js
+++ main/script/adScriptGogoanime.js
@@ -432,7 +432,7 @@ function mobileCheckAdScript() {
 // }
 
 function callFullHBAds(adCode, ub_slot) {
-    fillRefreshMap();
+    // fillRefreshMap();
     ubpbjs.que.push(function () {
         ubpbjs.requestBids({
             timeout: PREBID_TIMEOUT,

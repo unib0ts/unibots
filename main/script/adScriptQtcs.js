bf8d7dfa7b97aba7bee55a8e67c7474f27d028d6
bhu15dr <bhu15dr@gmail.com>
2021-12-24 14:24:49 +0530

    qtcs

--- main/script/adScriptQtcs.js
+++ main/script/adScriptQtcs.js
@@ -1,71 +1,3 @@
-// mybotBlockedPagesFlag = 1;
-// mybotBlockedUrl = 'https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/blocks/blocksDantri.json';
-// mybotBlockedClientName = 'dantri';
-//
-// if(typeof mybotBlockedPagesFlag !== 'undefined' && mybotBlockedPagesFlag ==1){
-//   urlToCheck = window.location.host+window.location.pathname;
-//
-//   var request = new XMLHttpRequest();
-//   url = 'https://api.unibots.in/block?client=Dantri&page='+urlToCheck;
-//
-//   request.open('GET', url, true);
-//   request.onload = function() {
-//     if (request.status >= 200 && request.status < 400) {
-//       var data = request.responseText;
-//       data = JSON.parse(data);
-//       // data = data[mybotBlockedClientName];
-//       if(data) {
-//         // data = data.urls;
-//         if(data.status == true){
-//           return false;
-//         }
-//         else{
-//           mybotubScript();
-//         }
-//       }
-//     }
-//     else {
-//       console.log('Block Check Request failed');
-//       mybotubScript();
-//     }
-//   };
-//   request.onerror = function() {
-//     console.log('Request failed');
-//     mybotubScript();
-//   };
-//   request.send();
-// }
-// else{
-//   mybotubScript();
-// }
-
-  // // load apstag.js library
-  // !function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
-  //
-  // var requestManager = {
-  //     adserverRequestSent: false,
-  //     aps: false,
-  //     prebid: false
-  // };
-  //
-  // //initialize the apstag.js library on the page to allow bidding
-  // apstag.init({
-  //
-  //      pubID: '8282b9c6-324d-4939-b1ea-958d67a9e637',
-  //      adServer: 'googletag',
-  //      schain: {
-  //           complete: 1,
-  //           ver:'1.0',
-  //           nodes: [
-  //             {
-  //                asi:'aps.amazon.com',
-  //                sid:'116', // Same seller_id for the publisher in sellers.json
-  //                hp:1
-  //              }
-  //           ],
-  //      }
-  // });
-  // apSlots = []
 
   mybotGACode = 'G-TVKXDF7QLT';
   mybotgabywa = '<script async src="https://www.googletagmanager.com/gtag/js?id='+mybotGACode+'"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "'+mybotGACode+'");</script>';
@@ -88,6 +20,45 @@ var check = false;
 (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
 return check;
 };
+
+var s0 = document.createElement('script');
+s0.src = "https://www.googletagservices.com/tag/js/gpt.js";
+s0.type = "text/javascript";
+document.getElementsByTagName('head')[0].appendChild(s0);
+
+var s1 = document.createElement('script');
+s1.async = "async";
+s1.src = "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/script/adScript.js";
+s1.type = "text/javascript";
+document.getElementsByTagName('head')[0].appendChild(s1);
+
+var GEO_CODE = '';
+(function (){
+  var request = new XMLHttpRequest();
+    url = 'https://pro.ip-api.com/json/?fields=status,message,countryCode&key=LWKtz4EzQwMJRyQ';
+    request.open('GET', url, true);
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        var data = request.responseText;
+        data = JSON.parse(data);
+        if(data.status == "success") {
+          GEO_CODE = data.countryCode;
+        }
+        else {
+          console.error("Geo Request Failed");
+        }
+      }
+      else {
+        console.error('Request failed from server');
+      }
+      mainHbRun();
+    };
+    request.onerror = function() {
+      console.error('Request failed to Reach GEO Server');
+      mainHbRun();
+    };
+    request.send();
+})();
 
 if (mobileCheck()) {
     z1= document.createElement('div');
@@ -172,58 +143,7 @@ var mappings = {
   renderedFlag: [false]
 };
 
-// apSlotTemp = {
-//   // slotID: mappings_full_hb_config.targetUnits[index],
-//   // slotName: mappings_full_hb_config.adUnitNames[index],
-//   // sizes: mappings_full_hb_config.sizes[index]
-//
-//   slotID: 'div-gpt-ad-1639122072424-0',
-//   slotName: '/22082859479,22547024640/qtcs.com.vn_anchor_320x50',
-//   sizes: mappings.sizes,
-// }
-// apSlots.push(apSlotTemp);
 
-
-
-// function mybotubScript() {
-  var s0 = document.createElement('script');
-  s0.src = "https://www.googletagservices.com/tag/js/gpt.js";
-  s0.type = "text/javascript";
-  document.getElementsByTagName('head')[0].appendChild(s0);
-
-  var s1 = document.createElement('script');
-  s1.async = "async";
-  s1.src = "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/main/script/adScript.js";
-  s1.type = "text/javascript";
-  document.getElementsByTagName('head')[0].appendChild(s1);
-
-  var GEO_CODE = '';
-  (function (){
-    var request = new XMLHttpRequest();
-  		url = 'https://pro.ip-api.com/json/?fields=status,message,countryCode&key=LWKtz4EzQwMJRyQ';
-  		request.open('GET', url, true);
-  		request.onload = function() {
-  			if (request.status >= 200 && request.status < 400) {
-  				var data = request.responseText;
-  				data = JSON.parse(data);
-  				if(data.status == "success") {
-            GEO_CODE = data.countryCode;
-  				}
-  				else {
-  					console.error("Geo Request Failed");
-  				}
-  			}
-  			else {
-  				console.error('Request failed from server');
-  			}
-        mainHbRun();
-  		};
-  		request.onerror = function() {
-  			console.error('Request failed to Reach GEO Server');
-        mainHbRun();
-  		};
-  		request.send();
-  })();
 
   function mainHbRun(){
     ubpbjs.que.push(function() {
@@ -325,13 +245,6 @@ var mappings = {
     mappings.adCode.push('/22082859479,22547024640/qtcs.com.vn_anchor_320x50');
     mappings.sizes.push(div_1_sizes);
     mappings.adId.push('div-gpt-ad-1639122072424-0');
-    // apSlotTemp = {
-    //   slotID: mappings.adId[0],
-    //   slotName: mappings.adCode[0],
-    //   sizes: mappings.sizes
-    // }
-    // apSlots.push(apSlotTemp);
-    // callAPStagBids(); //Ap part
     googletag.cmd.push(function() {
       googletag.pubads().addEventListener('slotRenderEnded', function(event) {
         if (event.slot === ub_slot1) {
@@ -394,93 +307,10 @@ function initAdserver() {
   googletag.cmd.push(function () {
     ubpbjs.que.push(function () {
       ubpbjs.setTargetingForGPTAsync();
-      requestManager.prebid = true;
-      biddersBack();
       googletag.pubads().refresh(mappings.slots);
     });
   });
-  // callAds(bids);
 }
-
-// function callAds(bids = {}) {
-//   let ubBidscheckFlag = false;
-//   if (Object.keys(bids).length === 0 && bids.constructor === Object){}
-//   else {
-//     bids[Object.keys(bids)].bids.forEach((bid) => {
-//       if (bid.cpm > 0.01) {
-//         ubBidscheckFlag = true;
-//       }
-//     })
-//   }
-//
-//   if (ubBidscheckFlag) {
-//     googletag.cmd.push(function () {
-//       ubpbjs.que.push(function () {
-//         ubpbjs.setTargetingForGPTAsync();
-//         googletag.pubads().refresh(mappings.slots);
-//         console.log('HB server request');
-//       });
-//     });
-//   }
-//   else{
-//     googletag.cmd.push(function () {
-//       googletag.pubads().refresh(mappings.slots);
-//       console.log('Only Google server request');
-//     });
-//   }
-// }
-
-
-// var botmanCalled = false;
-// var userStatusBM = '';
-// function callBotman(){
-//   if(userStatusBM == ''){
-//     var request = new XMLHttpRequest();
-//     var url = 'https://ep7.10777.api.botman.ninja/ic2.php?m=AF&t=prebid&s=10777&b=10777&s15=techz';
-//     request.open('GET', url, true);
-//     request.onload = function() {
-//       if (request.status >= 200 && request.status < 400) {
-//         var data = request.responseText;
-//         if(data != ""){
-//           data = JSON.parse(data);
-//           userStatusBM = data;
-//           if(userStatusBM == "0" || userStatusBM == "3"){
-//             callAdsUB();
-//           }
-//           else{
-//             console.log('Not Valid Traffic for openx');
-//           }
-//         }
-//         else{
-//           console.error('Data not returned from server');
-//           callAdsUB();
-//         }
-//       }
-//       else {
-//         console.error('Request failed from server');
-//         callAdsUB();
-//       }
-//     };
-//     request.onerror = function() {
-//       console.error('Request failed to Reach Server');
-//       callAdsUB();
-//     };
-//     request.send();
-//   }
-//   else{
-//     if(userStatusBM == "0" || userStatusBM == "3"){
-//       callAdsUB();
-//     }
-//     else{
-//       console.log('Not Valid Traffic for openx');
-//     }
-//   }
-//
-// }
-//
-// function callAdsUB(){
-//   // googletag.pubads().refresh(mappings.slots);
-// }
 
 function googleDefine(slotNumbers, adCode, sizes, adId){
   for(var i=0; i<slotNumbers.length;i++){
@@ -501,54 +331,6 @@ function googlePush(){
   });
 }
 
-// function callAPSAds(adCode, ub_slot){
-//   ubpbjs.que.push(function(){
-//     ubpbjs.requestBids({
-//       timeout: PREBID_TIMEOUT,
-//       adUnits: adUnits,
-//       adUnitCodes: adCode,
-//       bidsBackHandler: function() {
-//         // ubpbjs.initAdserverSetHB = true;
-//         googletag.cmd.push(function() {
-//           ubpbjs.que.push(function() {
-//               ubpbjs.setTargetingForGPTAsync();
-//               requestManager.prebid = true;
-//               biddersBack();
-//               // googletag.pubads().refresh(ub_slot);
-//           });
-//         });
-//       }
-//     });
-//   });
-// }
-function callAPStagBids(){
-  apstag.fetchBids({
-    slots: apSlots,
-     timeout: 2000
-  },function(bids) {
-          googletag.cmd.push(function() {
-              apstag.setDisplayBids();
-              requestManager.aps = true;
-              biddersBack();
-          });
-      }
-  );
-}
-function biddersBack() {
-    if (requestManager.aps && requestManager.prebid) {
-        sendAdserverRequest();
-    }
-    return;
-}
-function sendAdserverRequest() {
-    if (requestManager.adserverRequestSent === true) {
-        return;
-    }
-    requestManager.adserverRequestSent = true;
-    googletag.cmd.push(function() {
-        googletag.pubads().refresh(mappings.slots);
-    });
-}
 
 function loadAd(id, adUnits){
 	var elm = document.getElementById(id);

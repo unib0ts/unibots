a6eb8d5bfafe5be8038d54b882db7d3b375d36af
bhu15dr <bhu15dr@gmail.com>
2022-01-25 17:22:26 +0530

    cube pstn right

--- main/script/cubeScriptCelebhub.js
+++ main/script/cubeScriptCelebhub.js
@@ -14,7 +14,7 @@ function mobileCheck() {
     return check;
 }
 
-  var mybotstyleSheet ="#div-clmb-ctn-466738-1{width: auto !important;z-index: 2147483647 !important;} #div-clmb-ctn-466739-1{width: auto !important;z-index: 2147483647 !important;} #ifr_466738-1{bottom: 125px !important;left: -30px !important;} #ifr_466739-1{bottom: 160px !important;left: -30px !important;height: 180px !important;} #div-ub-1{z-index: 2147483647 !important;}";
+  var mybotstyleSheet ="#div-clmb-ctn-466738-1{width: auto !important;z-index: 2147483647 !important;} #div-clmb-ctn-466739-1{width: auto !important;z-index: 2147483647 !important;} #ifr_466738-1{bottom: 260px !important; right:0px !important;left: unset !important;height: 180px !important;} #ifr_466739-1{bottom: 160px !important;left: -30px !important;height: 180px !important;} #div-ub-1{z-index: 2147483647 !important;}";
 
   var css = document.createElement("style");
   css.type = "text/css";
@@ -35,7 +35,7 @@ function mobileCheck() {
     z1 = document.createElement("div");
     z1.id = "div-gpt-ad-1643007597943-0";
     if (!mobileCheck()) {
-      z1.style = "width:320px;height:50px;float:left;text-align:center;position:fixed;bottom:82px;left:5px;z-index:99";
+      z1.style = "width:320px;height:50px;float:right;text-align:center;position:fixed;bottom:210px;right:5px;z-index:99";
     }else {
       z1.style = "width:320px;height:50px;float:left;text-align:center;position:fixed;bottom:110px !important;left:5px;z-index:99";
     }

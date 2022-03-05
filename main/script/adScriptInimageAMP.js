d44ef86516bd8ae7825adb72f19dee24719171b2
bhu15dr <bhu15dr@gmail.com>
2022-03-05 14:22:32 +0530

    repo unused files removed

---
+++ main/script/adScriptInimageAMP.js
@@ -0,0 +1,67 @@
+<amp-iframe width="auto" height="66" layout="fixed-height" sandbox="allow-scripts allow-same-origin allow-popups" frameborder="0" src="https://adgebra.co.in/afpf/IBN_AMP.html?domain=amp-sakshi.com&amp;pubid=233&amp;slotId=1&amp;templateId=4675&amp;accessMode=1&amp;divId=div_407860734567731&amp;cache=123" style="margin-top: -57px; height: 66px; --loader-delay-offset:134ms !important;" class="i-amphtml-element i-amphtml-layout-fixed-height i-amphtml-layout-size-defined i-amphtml-built i-amphtml-layout" i-amphtml-layout="fixed-height">
+                  <amp-img layout="fill" src="https://cdn.adgebra.in/custom/generic/adgebra_icon.png" placeholder="" class="i-amphtml-element i-amphtml-layout-fill i-amphtml-layout-size-defined i-amphtml-built i-amphtml-layout amp-hidden" i-amphtml-layout="fill" i-amphtml-auto-lightbox-visited=""><img decoding="async" src="https://cdn.adgebra.in/custom/generic/adgebra_icon.png" class="i-amphtml-fill-content i-amphtml-replaced-content" i-amphtml-auto-lightbox-visited=""></amp-img>
+                  <i-amphtml-scroll-container class="amp-active"><iframe class="i-amphtml-fill-content" name="amp_iframe1" frameborder="0" allow="" sandbox="allow-scripts allow-same-origin allow-popups" src="https://adgebra.co.in/afpf/IBN_AMP.html?domain=amp-sakshi.com&amp;pubid=233&amp;slotId=1&amp;templateId=4675&amp;accessMode=1&amp;divId=div_407860734567731&amp;cache=123#amp=1" style="z-index: 0;"></iframe></i-amphtml-scroll-container></amp-iframe>
+
+.i-amphtml-layout-size-defined {
+    overflow: hidden!important;
+}
+
+.i-amphtml-layout-container, .i-amphtml-layout-fixed-height, [layout=container], [layout=fixed-height][height]:not(.i-amphtml-layout-fixed-height) {
+    display: block;
+    position: relative;
+}
+
+.i-amphtml-element>[placeholder].amp-hidden, .i-amphtml-element>[placeholder].hidden {
+    visibility: hidden;
+}
+
+.i-amphtml-layout-size-defined>[fallback], .i-amphtml-layout-size-defined>[placeholder] {
+    position: absolute!important;
+    top: 0!important;
+    left: 0!important;
+    right: 0!important;
+    bottom: 0!important;
+    z-index: 1;
+  }
+
+  .i-amphtml-element>[placeholder], [layout]:not(.i-amphtml-element)>[placeholder], [width][height][heights]:not([layout]):not(.i-amphtml-element)>[placeholder], [width][height][sizes]:not([layout]):not(.i-amphtml-element)>[placeholder] {
+    display: block;
+    line-height: normal;
+}
+
+.i-amphtml-layout-size-defined {
+    overflow: hidden!important;
+}
+
+i-amp-scroll-container.amp-active, i-amphtml-scroll-container.amp-active {
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+}
+
+i-amp-scroll-container, i-amphtml-scroll-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: block;
+}
+
+.i-amphtml-layout-size-defined .i-amphtml-fill-content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+}
+
+.i-amphtml-blurry-placeholder, .i-amphtml-fill-content {
+    display: block;
+    height: 0;
+    max-height: 100%;
+    max-width: 100%;
+    min-height: 100%;
+    min-width: 100%;
+    width: 0;
+    margin: auto;
+}

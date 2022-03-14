7123d945d377e9c132bab831ebb9c17fe130929c
bhu15dr <bhu15dr@gmail.com>
2022-03-14 17:04:44 +0530

    clmb refresh disbled

--- iplbowl65/script.js
+++ iplbowl65/script.js
@@ -148,21 +148,21 @@ function ubadScript() {
      }
    }
 
-   setInterval(function(){
-     if (ub_innerDoc.querySelector('#cube3dSwiper') !=null) {
-       if (!mobileCheck()) {
-         try{
-           colombia.refresh('div-clmb-ctn-466738-1');
-           cubespinnervisibility();
-         }catch(e){}
-       }else {
-         try{
-           colombia.refresh('div-clmb-ctn-466739-1');
-           cubespinnervisibility();
-         }catch(e){}
-       }
-      }
-   }, 30000);
+   // setInterval(function(){
+   //   if (ub_innerDoc.querySelector('#cube3dSwiper') !=null) {
+   //     if (!mobileCheck()) {
+   //       try{
+   //         colombia.refresh('div-clmb-ctn-466738-1');
+   //         cubespinnervisibility();
+   //       }catch(e){}
+   //     }else {
+   //       try{
+   //         colombia.refresh('div-clmb-ctn-466739-1');
+   //         cubespinnervisibility();
+   //       }catch(e){}
+   //     }
+   //    }
+   // }, 30000);
 
 
    function ub_refresh(clmbdiv_id, clmbiframe_id, ub_clmb ) {

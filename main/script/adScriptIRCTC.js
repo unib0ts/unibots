858a0f466302a1ca49cf01b372ba304c191cfb65
bhu15dr <bhu15dr@gmail.com>
2022-03-14 13:28:17 +0530

    script upd

--- main/script/adScriptIRCTC.js
+++ main/script/adScriptIRCTC.js
@@ -1,5 +1,10 @@
 var s0 = document.createElement('script');
-s0.async = "async";
-s0.src = "https://cdn.unibots.in/headerbidding/irctc.js";
-s0.type = "text/javascript";
-document.getElementsByTagName('head')[0].appendChild(s0);
+	s0.async = "async";
+	s0.src = "https://cdn.jsdelivr.net/gh/unib0ts/unibots@latest/ubHB/main/hb.js";
+	s0.type = "text/javascript";
+	document.getElementsByTagName('head')[0].appendChild(s0);
+
+	window.unibotshb = window.unibotshb || { cmd: [] };
+	unibotshb.cmd.push(() => {
+	    ubHB("irctc");
+	});

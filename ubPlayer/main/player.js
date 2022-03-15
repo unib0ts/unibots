c0d367773e19d23540e91111ba973136eebf42ae
Arun Kumar <arunkumarjangra@gmail.com>
2022-03-15 11:05:14 +0530

    diff script for deepika, new policy features

--- ubPlayer/main/player.js
+++ ubPlayer/main/player.js
@@ -1,5 +1,10 @@
+var clientDomain = location.hostname.split('.').reverse().splice(0, 2).reverse().join('.');
 var scriptElement = document.createElement('script');
 scriptElement.async = "async";
-scriptElement.src = "https://cdn.unibots.in/ubPlayer/player.js";
+if(clientDomain == "deepika.com"){
+    scriptElement.src = "https://cdn.unibots.in/clientData/player.js";
+}else{
+    scriptElement.src = "https://cdn.unibots.in/ubPlayer/player.js";
+}
 scriptElement.type = "text/javascript";
 document.getElementsByTagName('head')[0].appendChild(scriptElement);

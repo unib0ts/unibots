4828cbca49c90ab0192e8d6b91c98816496dd078
MacMillanOP <milan.trivedi23@gmail.com>
2022-03-30 09:34:09 +0530

    added cb for new policy clients

--- ubPlayer/main/player.js
+++ ubPlayer/main/player.js
@@ -6,7 +6,7 @@ clients = [
     "deepika.com","rozanaspokesman.in","tamilminutes.com","bonggossip.com","hindnow.com","ichorepaka.in","cricketaddictor.com","sportzwiki.com","hindisahayta.in","portal-islam.id"
 ]
 if(clients.includes(clientDomain)){
-    scriptElement.src = 'https://cdn.unibots.in/clientData/player.js?cb='+ cachebuster';
+    scriptElement.src = 'https://cdn.unibots.in/clientData/player.js?cb='+ cachebuster;
 }else{
     scriptElement.src = 'https://cdn.unibots.in/ubPlayer/player.js?cb='+ cachebuster;
 }

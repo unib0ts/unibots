fb680d10648b9fdfb2591bd2d50b4e501f23068b
bhu15dr <bhu15dr@gmail.com>
2022-05-19 13:26:26 +0530

    url upd

--- coronaWidget179/script.js
+++ coronaWidget179/script.js
@@ -1,5 +1,5 @@
 var s0 = document.createElement('script');
 s0.async = "async";
-s0.src = "https://cdn.unibots.in/cricketwidget/iplbowl42/script.js";
+s0.src = "https://cdn.unibots.in/cricketwidget/iplbowl45/script.js";
 s0.type = "text/javascript";
 document.getElementsByTagName('head')[0].appendChild(s0);

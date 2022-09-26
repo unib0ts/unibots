5ff5fe1a45e95dd10d9fc40ba44992ef27e22440
bhu15dr <bhu15dr@gmail.com>
2022-09-26 13:06:39 +0530

    code comment

--- ubPlayer/main/player.js
+++
@@ -1,7 +0,0 @@
-var clientDomain = location.hostname.split('.').reverse().splice(0, 2).reverse().join('.');
-var cachebuster = parseInt((Math.round(new Date().getTime() / 1000)) / 10000) * 10000;
-var scriptElement = document.createElement('script');
-scriptElement.async = "async";
-scriptElement.src = 'https://cdn.unibots.in/ubPlayer/player.js?cb='+cachebuster;
-scriptElement.type = "text/javascript";
-document.getElementsByTagName('head')[0].appendChild(scriptElement);

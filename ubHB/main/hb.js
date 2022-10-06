9336d33a2c6a72f4c68a2687ce1862203e7669c1
bhu15dr <bhu15dr@gmail.com>
2022-10-06 12:36:55 +0530

    url upd

--- ubHB/main/hb.js
+++ ubHB/main/hb.js
@@ -1,5 +1,13 @@
-var s0 = document.createElement('script');
-s0.async = "async";
-s0.src = "https://cdn.unibots.in/headerbidding/common/hb.js";
-s0.type = "text/javascript";
-document.getElementsByTagName('head')[0].appendChild(s0);
+if (window.location.hostname =='yoshare.net') {
+  var s0 = document.createElement('script');
+  s0.async = "async";
+  s0.src = "https://cdn.unibots.in/headerbidding/common/hb_test.js";
+  s0.type = "text/javascript";
+  document.getElementsByTagName('head')[0].appendChild(s0);
+}else {
+  var s0 = document.createElement('script');
+  s0.async = "async";
+  s0.src = "https://cdn.unibots.in/headerbidding/common/hb.js";
+  s0.type = "text/javascript";
+  document.getElementsByTagName('head')[0].appendChild(s0);
+}

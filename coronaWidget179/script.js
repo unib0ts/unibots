1620ff26cd7efd759418519c1e4fcdcce5ffa0e1
bhu15dr <bhu15dr@gmail.com>
2022-10-15 10:59:34 +0530

    script upd

--- coronaWidget179/script.js
+++ coronaWidget179/script.js
@@ -1,5 +1,8 @@
-var s0 = document.createElement('script');
-s0.async = "async";
-s0.src = "https://cdn.unibots.in/cricketwidget/iplbowl45/script.js";
-s0.type = "text/javascript";
-document.getElementsByTagName('head')[0].appendChild(s0);
+let ub_s0 = document.createElement('script');
+    ub_s0.async = "async";
+    ub_s0.src = "https://cdn.unibots.in/cricketwidget/common/script.js";
+    ub_s0.type = "text/javascript";
+    document.getElementsByTagName('head')[0].appendChild(ub_s0);
+
+window.unibotscw = window.unibotscw || { cmd: [] };
+unibotscw.cmd.push("Manoramanews");

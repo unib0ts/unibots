35ae675948b68591b66822106bfea73e4a018072
bhu15dr <bhu15dr@gmail.com>
2022-11-17 12:38:10 +0530

    assets

---
+++ main/quiz/Assets/BlogData.js
@@ -0,0 +1,183 @@
+import Plans from "./blogs/plans.jpg"
+import Fish from "./blogs/fish.jpg"
+import Girl from "./blogs/girl.jpg"
+import NewYork from "./blogs/newyork.jpg"
+import Hippos from "./blogs/hippos.jpg"
+
+import Peoples from "./blogs/peoples.jpg"
+import Fish2 from "./blogs/fish2.jpg"
+import Moqu from "./blogs/mosq.jpg"
+import Galaxy from "./blogs/galaxy.jpg"
+import Fish3 from "./blogs/fish3.jpg"
+import Planets from "./blogs/planets.jpg"
+import Rocket from "./blogs/rocket.jpg"
+import Police from "./blogs/police.jpg"
+import Moon from "./blogs/moon.jpg"
+import Pawri from "./blogs/pawri.png"
+import Driver from "./blogs/driver.jpg"
+import IndiaUsa from "./blogs/indiausa.jpg"
+import Bahubali from "./blogs/bahubali.png"
+import CatMan from "./blogs/catman.jpg"
+import Amitab from "./blogs/amitab.jpg"
+
+const BlogData = [
+    {
+        title: '5 Facts That Will Blow Your Mind!',
+        para: [
+            {
+                text: "The world is a funny, weird, quirky, interesting place. All around us, there are extremely interesting things which often miss our eyes. But a little digging around and you find your unearthing fantastic facts and secrets which are simply mindblowing!"
+            },
+            {
+                text: "Here in this article, we have put together a list of 5 such facts! Enjoy!"
+            }
+        ],
+        numbers: [
+            {
+                text: "The second largest air force in the world is the US Navy! And the first, of course, is the US Air Force itself.",
+                img: Plans
+            },
+            {
+                text: "Sharks are older than trees! And the Sharks have existed for more than 450 million years, whereas trees first started growing on earth around 350 million years ago.",
+                img: Fish
+            },
+            {
+                text: "It's just impossible to hum if you hold your nose! Go ahead, try it.",
+                img: Girl
+            },
+            {
+                text: "New York was called New Amsterdam earlier! In 1664 the English took over New Amsterdam and renamed it New York City after the Duke of York.",
+                img: NewYork
+            },
+            {
+                text: "Hippos run faster than the fastest human! Hippos run at about 48 kms/h whereas the fastest speed that Usain Bolt ever ran at was under 45 kms/h.",
+                img: Hippos
+            },
+        ],
+        options: [],
+    },
+    {
+        title: '9 Random Facts that’ll make you go “What the ….”!',
+        para: [
+            {
+                text: "The world is full of very bizarre and mind wobbling things. Here are some of the facts we pulled together which will make you question your own existence!"
+            }
+        ],
+        numbers: [
+            {
+                text: "Competitive art used to be in the Olympics. Between 1912 and 1948, the Olympic Games awarded medals in sculpture, music, painting, and architecture.",
+                img: Peoples
+            },
+            {
+                text: "If placed in darkness or running water, goldfish lose their colour. Do not try this with your pet goldfish.",
+                img: Fish2
+            },
+            {
+                text: "Mosquitoes are three times more attracted to blue light than any other colour. Now this is something useful when you plan for your next camping trip.",
+                img: Moqu
+            },
+            {
+                text: "In one cubic millimeter of brain tissue, there are more neural connections than there are stars in the Milky Way galaxy. But still we are not that bright.",
+                img: Galaxy
+            },
+            {
+                text: "The giant squid's brain is a doughnut shape, and its food pipe runs through it. If the squid eats anything too big, it will suffer massive brain damage. Now that’s evolution!",
+                img: Fish3
+            },
+            {
+                text: "From when it was first discovered till the time it was declassified as a planet, Pluto had still not made a full orbit around the sun. Poor Pluto!",
+                img: Planets
+            },
+            {
+                text: "The Apollo 11 Lander computer crashed and restarted several times on the way down to the Moon. And yet, this was not the most dangerous part of their descent.",
+                img: Rocket
+            },
+            {
+                text: "The first person convicted of speeding was going eight mph. On Jan. 28, 1896, Arnold was spotted going four times the speed limit in his 19th-century Benz.",
+                img: Police
+            },
+            {
+                text: "The moon is (slowly) slowing the Earth's rotation. Every one hundred years, the moon adds approximately 1.4 milliseconds to a day.",
+                img: Moon
+            },
+        ]
+    },
+    {
+        title: 'Most trending Memes of 2021!',
+        para: [
+            {
+                heading: "",
+                text: "The world of internet is full of creative people who can turn anything into a funny meme and can make it viral. Here are some of the memes that got trending in the last few months.",
+                img: ""
+            },
+            {
+                heading: "‘Pawri’ girl",
+                text: "You must be living under a rock or something if you haven’t heard about the Pawri Girl. Internet or no internet, almost everyone knows her. The girl in the video is a famous Pakistani influencer and content creator Dananeer. According to her it was supposed to be a very random video where she wanted to capture the fun moment she was sharing with her friends. But the internet had other plans for her…",
+                img: Pawri
+            },
+            {
+                heading: "Heavy driver",
+                text: "This is a 19 second video where someone is trying to shoot their own version of fast and furious and smoothly parked their SUV in a ditch. What is even more interesting and funny in this video than the stunt, is the commentary of the person recording the video and awarding the title of Heavy Driver to the person. This audio got trended and was been used by other meme creators in their own versions.",
+                img: Driver
+            },
+            {
+                heading: "US vs India",
+                text: "The meme which comes in a text-tweet format is a division by countries, India vs the USA. It's represented by flags, with the USA flag first stating a normal scenario, and then an Indian flag explaining how different the same scenario in a desi country would look like. People of the internet poured their creativity and came up with some great versions of it.",
+                img: IndiaUsa
+            },
+            {
+                heading: "Vibing cat meme",
+                text: "If you have been following latest trending videos that went viral this year, the popular “vibing cat” meme where visually impaired Turkish musician Bilal Göregen is seen playing darbuka on a bench while an animated cat nods its head to the music is sure to have caught your attention. Later the musicians came up with other tracks covers for his fans in India.",
+                img: CatMan
+            },
+            {
+                heading: "Exam postponed memes",
+                text: "The decision of canceling of class 10th exams and postponing of class 12th exam of CBSE Board was announced by the central government due to the current situation of COVID 19 in India. This brought a sense of relief to the students but for it rained memes on all social media channels. People are coming up with their own ideas of memes.",
+                img: Bahubali
+            }
+        ]
+    },
+    {
+        title: "On Amitabh Bachchan's Birthday, Here Are 5 Lesser-Known Facts About Him",
+        para: [
+            {
+                test: "The star of the millennium, Amitabh Bachchan celebrates his birthday every year on October 11. With over 200 Bollywood movies to his credit, he is hands-down the legend of Indian cinema. But the journey to his huge success wasn't a cakewalk for Bachchan. He has had his own struggles and failures in life, but with grit, determination and hard work he made his dreams come true.",
+                img: ""
+
+            },
+            {
+                text: "We list below 5 facts about Sr Bachchan that makes him the star and personality he is:",
+                img: Amitab
+            },
+
+        ],
+        options: [
+            {
+                text: "Amitabh Bachchan is the only actor to have done a double role in 14 movies and a triple role in one movie",
+            },
+            {
+                text: "He suffers from a rare muscular disorder called myasthenia gravis that causes varying degrees of skeletal muscle weakness"
+            },
+            {
+                text: "Bachchan originally wanted to pursue engineering and join the Indian Air Force, but eventually turned to acting in movies and the rest is history!"
+            },
+            {
+                text: "Amitabh Bachchan gave 12 consecutive flop films before delivering his first hit Zanjeer in 1973. During his days of struggle, actor/director Mehmood who was his mentor back then offered him a place to live in his own house. Amitabh's first salary was just Rs 300."
+            },
+            {
+                text: "Despite being the superstar of his time, Amitabh Bachchan went bankrupt in the 1990s. He asked for a job from Yash Chopra and hence bagged a role in the movie Mohabbatein. He slowly and steadily bounced back to success and financial stability."
+            }
+        ]
+    },
+    // {
+    //     title: 'Happy Independence Day - 5 Lesser-Known Freedom Fighters of India'
+    // },
+    // {
+    //     title: '5 Breath-taking facts about Qutub Minar - The tallest brick minaret in the world'
+    // },
+    // {
+    //     title: '6 Mysterious Facts About The Unscalable Mount Kailash'
+    // }
+]
+
+
+export default BlogData
---
+++ main/quiz/Assets/CardData.js
@@ -0,0 +1,56 @@
+import Coin from "./coin.svg"
+import Cricket from "./cricket.png"
+import Automobiles from "./automobile.png"
+import Hollywood from "./hollywood.png"
+import History from "./history.png"
+import Banking from "./banking.png"
+
+const CardData = [
+  {
+    name: 'IPS',
+    announcement: '03:55 AM',
+    price: "1,00,000",
+    fee: "75",
+    users: '2,002',
+    live: true,
+    img: Cricket
+  },
+  {
+    name: 'HISTORY',
+    announcement: '06:55 PM',
+    price: "2,50,000",
+    fee: "99",
+    users: '3,402',
+    live: true,
+    img: History
+  },
+  {
+    name: 'FINANCE & BANKING',
+    announcement: '09:55 PM',
+    price: "2,57,900",
+    fee: "453",
+    users: '13,402',
+    live: true,
+    img: Banking
+  },
+  {
+    name: 'HOLLYWOOD',
+    announcement: '08:00 AM',
+    price: "4,57,900",
+    fee: "1453",
+    users: '802',
+    live: true,
+    img: Hollywood
+  },
+  {
+    name: 'AUTOMOBILE',
+    announcement: '10:10 AM',
+    price: "1,57,900",
+    fee: "70",
+    users: '77',
+    live: true,
+    img: Automobiles
+  },
+]
+
+export default CardData

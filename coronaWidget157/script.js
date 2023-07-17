718a5b3e44a67b5d4f6e3f91f2ff5a39c81e89bd
bhu15dr <bhu15dr@gmail.com>
2023-07-17 15:56:06 +0530

    widget enb

--- coronaWidget157/script.js
+++ coronaWidget157/script.js
@@ -1,1 +1,11 @@
-console.log('script disabled');
+// console.log('script disabled');
+if(window.location.hostname === "phunusuckhoe.giadinhonline.vn"){
+    var ub_s0 = document.createElement('script');
+    ub_s0.async = "async";
+    ub_s0.src = "https://cdn.unibots.in/newsbot/common/script.js";
+    ub_s0.type = "text/javascript";
+     document.getElementsByTagName('head')[0].appendChild(ub_s0);
+    
+    window.unibotsna = window.unibotsna || { cmd: [] };
+    unibotsna.cmd.push("phunusuckhoe");
+}

82da39ab102c5949ef1179ad67daa94731eddb61
bhu15dr <bhu15dr@gmail.com>
2023-07-26 10:42:17 +0530

    hd script enb

--- coronaWidget157/script.js
+++ coronaWidget157/script.js
@@ -2,10 +2,10 @@
 if(window.location.hostname === "phunusuckhoe.giadinhonline.vn"){
     var ub_s0 = document.createElement('script');
     ub_s0.async = "async";
-    ub_s0.src = "https://cdn.unibots.in/newsbot/common/script.js";
+    ub_s0.src = "https://cdn.unibots.in/headerbidding/common/hb.js";
     ub_s0.type = "text/javascript";
      document.getElementsByTagName('head')[0].appendChild(ub_s0);
     
-    window.unibotsna = window.unibotsna || { cmd: [] };
-    unibotsna.cmd.push("phunusuckhoe");
+     window.unibotshb = window.unibotshb || { cmd: [] };
+     unibotshb.cmd.push(()=>{ ubHB("phunusuckhoe"); });
 }
